dc3286ca6729c26ec7573ca7b479d80575e7c541
ErickIgles <erick.almeida2125@gmail.com>
2025-08-04 09:00:59 -0300

    feat: adiciona modelo Tanque, Bomba e Abastecimento ao admin

--- abastecimento/admin.py
+++ abastecimento/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tanque, Bomba, Abastecimento
+
+
+@admin.register(Tanque)
+class TanqueAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tipo_combustivel', 'quantidade']
+    list_display_links = ['id', 'tipo_combustivel', 'quantidade']
+
+
+@admin.register(Bomba)
+class BombaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome_bomba', 'tanque']
+    list_display_links = ['id', 'nome_bomba', 'tanque']
+
+
+@admin.register(Abastecimento)
+class AbastecimentoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'bomba', 'litros_abastecidos', 'valor']
+    list_display_links = ['id', 'bomba', 'litros_abastecidos', 'valor']
+
