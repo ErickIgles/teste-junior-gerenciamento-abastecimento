10ed8b0d711c5647bba59be072fda8d0d559b935
ErickIgles <erick.almeida2125@gmail.com>
2025-08-04 10:30:45 -0300

    feat: adicionar views de criação, listagem, atualização e exclusão para Tanque

--- abastecimento/views.py
+++ abastecimento/views.py
@@ -4,12 +4,41 @@ from django.shortcuts import render
 from django.shortcuts import render
 
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView, UpdateView
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView
 
-from .models import Abastecimento, Tanque
-from .forms import AbastecimentoForm, TanqueForm
+from .models import Abastecimento, Tanque, Bomba
+from .forms import AbastecimentoForm, TanqueForm, BombaForm
 
 
 class IndexTemplateView(TemplateView):
     template_name = 'abastecimento/base.html'
+
+
+class CreateTanqueView(CreateView):
+    model = Tanque
+    form_class = TanqueForm
+    template_name = 'abastecimento/tanque_form.html'
+    success_url = reverse_lazy('index')
+
+
+class ListaTanqueView(ListView):
+    model = Tanque
+    queryset = Tanque.objects.all()
+    context_object_name = 'tanques'
+    template_name = 'abastecimento/tanque_lista.html'
+
+
+class AtualizarTanqueView(UpdateView):
+    model = Tanque
+    form_class = TanqueForm
+    context_object_name = 'tanque'
+    template_name = 'abastecimento/tanque_form.html'
+    success_url = reverse_lazy('listagem_tanque')
+
+
+class DeletarTanqueView(DeleteView):
+    model = Tanque
+    context_object_name = 'tanque'
+    template_name = 'abastecimento/tanque_form_delete.html'
+    success_url = reverse_lazy('listagem_tanque')
 
