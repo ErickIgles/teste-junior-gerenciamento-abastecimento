72de9747b02a82fcb4079d65c24ed8c7f8e28e22
ErickIgles <erick.almeida2125@gmail.com>
2025-08-04 13:09:36 -0300

    feat: adicionar views para criação, listagem, atualização e deleção de Bombas; atualizar success_url na view de criação de tanque

--- abastecimento/views.py
+++ abastecimento/views.py
@@ -18,7 +18,7 @@ class CriarTanqueView(CreateView):
     model = Tanque
     form_class = TanqueForm
     template_name = 'abastecimento/tanque_form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('listagem_tanque')
 
 
 class ListaTanqueView(ListView):
@@ -41,5 +41,44 @@ class DeletarTanqueView(DeleteView):
     context_object_name = 'tanque'
     template_name = 'abastecimento/tanque_form_delete.html'
     success_url = reverse_lazy('listagem_tanque')
+
+
+class CriarBombaView(CreateView):
+    model = Bomba
+    form_class = BombaForm
+    template_name = 'abastecimento/bomba_form.html'
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tanques'] = Tanque.objects.all()
+        return context
+
+
+class ListaBombaView(ListView):
+    model = Bomba
+    queryset = Bomba.objects.all()
+    context_object_name = 'bombas'
+    template_name = 'abastecimento/bomba_lista.html'
+
+
+class AtualizarBombaView(UpdateView):
+    model = Bomba
+    form_class = BombaForm
+    context_object_name = 'bomba'
+    template_name = 'abastecimento/bomba_form.html'
+    success_url = reverse_lazy('listagem_bomba')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tanques'] = Tanque.objects.all()
+        return context 
+
+
+class DeletarBombaView(DeleteView):
+    model = Bomba
+    context_object_name = 'bomba'
+    template_name = 'abastecimento/bomba_form_delete.html'
+    success_url = reverse_lazy('listagem_bomba')
 
 
