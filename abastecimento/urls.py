576ed00d99477988e651785567409285f0606b4f
ErickIgles <erick.almeida2125@gmail.com>
2025-08-04 10:31:53 -0300

    feat: adicionar rotas para as views de criação, listagem, atualização e exclusão de Tanque

--- abastecimento/urls.py
+++ abastecimento/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import (
     IndexTemplateView,
+    CreateTanqueView,
+    ListaTanqueView,
+    AtualizarTanqueView,
+    DeletarTanqueView
 )
 
 urlpatterns = [
     path('', IndexTemplateView.as_view(), name='index'),
+    path('criar/tanque/', CreateTanqueView.as_view(), name='criar_tanque'),
+    path('listagem/tanque/', ListaTanqueView.as_view(), name='listagem_tanque'),
+    path('atualizar/tanque/<int:pk>/', AtualizarTanqueView.as_view(), name='atualizar_tanque'),
+    path('deletar/tanque/<int:pk>/', DeletarTanqueView.as_view(), name='deletar_tanque'),
 ]
