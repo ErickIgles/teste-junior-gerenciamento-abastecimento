2b802db3221b06064678ba7d3bc29246a1764ae2
ErickIgles <erick.almeida2125@gmail.com>
2025-08-04 09:04:14 -0300

    feat: adiciona View para a renderização de página inicial

--- abastecimento/views.py
+++ abastecimento/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView
+
+from .models import Abastecimento, Tanque
+from .forms import AbastecimentoForm, TanqueForm
+
+
+class IndexTemplateView(TemplateView):
+    template_name = 'abastecimento/base.html'
+
