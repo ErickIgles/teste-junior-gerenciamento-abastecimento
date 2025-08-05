7e1b4c99fd2c2e19e2b417aef3206bf997fdc5bf
ErickIgles <erick.almeida2125@gmail.com>
2025-08-05 11:38:19 -0300

    feat: implementar método clean_valor para tratar valores com vírgula e garantir conversão correta para Decimal no formulário de Abastecimento

--- abastecimento/forms.py
+++ abastecimento/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from decimal import Decimal
 from .models import Abastecimento, Tanque, Bomba
 
 
@@ -8,6 +8,26 @@ class AbastecimentoForm(forms.ModelForm):
     class Meta:
         model = Abastecimento
         fields = ['bomba', 'litros_abastecidos', 'valor']
+
+    
+    def clean_valor(self):
+
+        valor = self.cleaned_data.get('valor')
+
+        if isinstance(valor, str):
+            
+            valor_decimal = 0
+            valor_corrigido = valor.replace(',', '.')
+
+            if valor.count(','):
+                valor_decimal = Decimal(valor_corrigido)
+                return valor_decimal
+            else:
+                valor_decimal = Decimal(valor_corrigido)
+                return valor_decimal
+        return valor
+
+
 
 
 class TanqueForm(forms.ModelForm):
