130cba857e23f46fd6726e893543ea1a88c1a224
ErickIgles <erick.almeida2125@gmail.com>
2025-08-06 11:41:54 -0300

    chore: desativa temporariamente as rotas de cadastros

--- gerenciamento_abastecimento/urls.py
+++ gerenciamento_abastecimento/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('abastecimento.urls')),
-    path('cadastros/', include('cadastros.urls')),
+    # path('cadastros/', include('cadastros.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
