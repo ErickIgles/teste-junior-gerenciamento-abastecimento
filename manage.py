840130a591e58501a91dca52385c46e6b4fecec1
ErickIgles <erick.almeida2125@gmail.com>
2025-08-06 08:38:39 -0300

    chore: adiciona src ao sys.path em manage.py

--- manage.py
+++ manage.py
@@ -6,6 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
+
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciamento_abastecimento.settings')
     try:
         from django.core.management import execute_from_command_line
