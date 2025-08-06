aa81efdfe608f585607902d2f8c9093af6848688
ErickIgles <erick.almeida2125@gmail.com>
2025-08-06 17:41:48 -0300

    feat: trata vírgulas como separador decimal nos campos capacidade e quantidade

--- src/cadastros/tanques/forms.py
+++ src/cadastros/tanques/forms.py
@@ -1,10 +1,41 @@
 from django import forms
 from .models import Tanque
-
+from decimal import Decimal
 
 class TanqueForm(forms.ModelForm):
     
     class Meta:
         model = Tanque
         fields = ['tipo_combustivel', 'capacidade_maxima', 'quantidade_disponivel']
+
+    def clean_capacidade_maxima(self):
+        capacidade_maxima_decimal = 0
+
+        capacidade_maxima = self.cleaned_data.get('capacidade_maxima')
+
+        if isinstance(capacidade_maxima, str):
+            
+            if capacidade_maxima.count(','):
+                capacidade_maxima_corrigida = capacidade_maxima.replace(',', '.')
+
+                capacidade_maxima_decimal = Decimal(capacidade_maxima_corrigida)
+                return capacidade_maxima_decimal
+
+        return capacidade_maxima
+    
+
+
+    def clean_quantiade_disponivel(self):
+        quantidade_disponivel_decimal = 0
+
+        quantidade_disponivel = self.cleaned_data.get('quantidade_disponivel')
+
+        if isinstance(quantidade_disponivel, str):
+
+            if quantidade_disponivel.count(','):
+                quantidade_disponivel_corrigida = quantidade_disponivel.replace(',', '.')
+                quantidade_disponivel_decimal = Decimal(quantidade_disponivel_corrigida)
+                return quantidade_disponivel_decimal
+        
+        return quantidade_disponivel
 
