146223da3c83c76b014e0f4adedae08ff1e86b4a
ErickIgles <erick.almeida2125@gmail.com>
2025-08-06 17:08:03 -0300

    feat: adiciona modelo Bomba com relação a Tanque

--- src/cadastros/bombas/models.py
+++ src/cadastros/bombas/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+from core.models import Base
+from ..tanques.models import Tanque
+
+
+class Bomba(Base):
+    nome_bomba = models.CharField(verbose_name='Nome', max_length=144)
+    tanque = models.ForeignKey(Tanque, verbose_name='Tanque', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Bomba'
+        verbose_name_plural = 'Bombas'
+    
+    def __str__(self):
+        return f'{self.nome_bomba} - {self.tanque.tipo_combustivel}'
+
