0b5a16c640233a51d97e39d3df495dcf59440c03
ErickIgles <erick.almeida2125@gmail.com>
2025-08-06 18:32:29 -0300

    feat: adiciona rotas para views de CRUD de bombas

--- src/cadastros/bombas/urls.py
+++ src/cadastros/bombas/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 
-from . import views
+from .views import (
+    BombaCadastroView,
+    BombaListarView,
+    BombaAtualizarView,
+    BombaDeletarView
+)
 
 app_name='bombas'
 
+urlpatterns = [
+    path('cadastrar/', BombaCadastroView.as_view(), name='cadastrar'),
+    path('listagem/', BombaListarView.as_view(), name='listar'),
+    path('atualizar/<int:pk>/', BombaAtualizarView.as_view(), name='atualizar'),
+    path('deletar/<int:pk>/', BombaDeletarView.as_view(), name='deletar'),
+]
