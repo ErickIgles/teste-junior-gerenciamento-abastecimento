80501d2c35ca47126365dfa8d17ce97aaeb0c80b
ErickIgles <erick.almeida2125@gmail.com>
2025-08-06 18:29:01 -0300

    feat: registra o modelo Bomba no Django admin

--- src/cadastros/bombas/admin.py
+++ src/cadastros/bombas/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
 
+from .models import Bomba
+
+@admin.register(Bomba)
+class BombaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome_bomba', 'tanque']
+    list_display_links = ['id', 'nome_bomba', 'tanque']
+
