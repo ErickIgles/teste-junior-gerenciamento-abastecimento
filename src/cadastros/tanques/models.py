5a7333545726b7a640de8dbdb1c7582f0e637093
ErickIgles <erick.almeida2125@gmail.com>
2025-08-06 16:30:53 -0300

    feat: adiciona modelo Tanque com tipo, capacidade e quantidade

--- src/cadastros/tanques/models.py
+++ src/cadastros/tanques/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+from core.models import Base
+
+
+
+
+class Tanque(Base):
+    TIPO_COMBUSTIVEL_CHOICES = [
+        ('GASOLINA', 'Gasolina'),
+        ('DIESEL', 'Óleo Diesel')
+    ]
+
+    tipo_combustivel = models.CharField(verbose_name='Tipo de Combustivel', max_length=10, choices=TIPO_COMBUSTIVEL_CHOICES)
+    capacidade_maxima = models.DecimalField(verbose_name='Capacidade Máxima', max_digits=10, decimal_places=2)
+    quantidade_disponivel = models.DecimalField(verbose_name='Quantidade disponivel', max_digits=10, decimal_places=2, default=0.00)
+
+    class Meta:
+        verbose_name = 'Tanque'
+        verbose_name_plural = 'Tanques'
+    
+    def __str__(self):
+        return f'{self.tipo_combustivel}'
+
