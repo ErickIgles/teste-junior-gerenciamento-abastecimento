c233cd10774005e44f437647b2564714c15f69f3
ErickIgles <erick.almeida2125@gmail.com>
2025-08-07 16:41:57 -0300

    refactor: remove validações manuais e adicionar widgets e labels customizados no TanqueForm

--- src/cadastros/tanques/forms.py
+++ src/cadastros/tanques/forms.py
@@ -1,41 +1,22 @@
 from django import forms
 from .models import Tanque
-from decimal import Decimal
 
 class TanqueForm(forms.ModelForm):
     
     class Meta:
-        model = Tanque
-        fields = ['tipo_combustivel', 'capacidade_maxima', 'quantidade_disponivel']
-
-    def clean_capacidade_maxima(self):
-        capacidade_maxima_decimal = 0
-
-        capacidade_maxima = self.cleaned_data.get('capacidade_maxima')
-
-        if isinstance(capacidade_maxima, str):
-            
-            if capacidade_maxima.count(','):
-                capacidade_maxima_corrigida = capacidade_maxima.replace(',', '.')
-
-                capacidade_maxima_decimal = Decimal(capacidade_maxima_corrigida)
-                return capacidade_maxima_decimal
-
-        return capacidade_maxima
-    
-
-
-    def clean_quantiade_disponivel(self):
-        quantidade_disponivel_decimal = 0
-
-        quantidade_disponivel = self.cleaned_data.get('quantidade_disponivel')
-
-        if isinstance(quantidade_disponivel, str):
-
-            if quantidade_disponivel.count(','):
-                quantidade_disponivel_corrigida = quantidade_disponivel.replace(',', '.')
-                quantidade_disponivel_decimal = Decimal(quantidade_disponivel_corrigida)
-                return quantidade_disponivel_decimal
         
-        return quantidade_disponivel
-
+        model = Tanque
+        fields = ['tipo_combustivel', 'identificador_tanque', 'capacidade_maxima', 'quantidade_disponivel']
+        widgets = {
+            'tipo_combustivel': forms.Select(attrs={'class': 'form__select'}),
+            'identificador_tanque': forms.TextInput(attrs={'class': 'form__input'}),
+            'capacidade_maxima': forms.NumberInput(attrs={'class': 'form__input'}),
+            'quantidade_disponivel': forms.NumberInput(attrs={'class': 'form__input'}),
+        }
+        labels = {
+            'tipo_combustivel': 'Tipo de Combustível',
+            'identificador_tanque': 'Identificador',
+            'capacidade_maxima': 'Capacidade do Tanque',
+            'quantidade_disponivel': 'Quantidade Disponível',
+        }
+    
