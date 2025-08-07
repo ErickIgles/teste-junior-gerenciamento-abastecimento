3fe1de5a5cb431d559bcf193e2dd7c3a5a3935ca
ErickIgles <erick.almeida2125@gmail.com>
2025-08-07 16:40:12 -0300

    feat: adicionar campo identificador_tanque à exibição da lista no admin de Tanque

--- src/cadastros/tanques/admin.py
+++ src/cadastros/tanques/admin.py
@@ -5,5 +5,5 @@ from .models import Tanque
 
 @admin.register(Tanque)
 class TanqueAdmin(admin.ModelAdmin):
-    list_display = ['id', 'tipo_combustivel', 'capacidade_maxima', 'quantidade_disponivel']
-    list_display_links = ['id', 'tipo_combustivel', 'capacidade_maxima', 'quantidade_disponivel']
+    list_display = ['id', 'tipo_combustivel', 'identificador_tanque', 'capacidade_maxima', 'quantidade_disponivel']
+    list_display_links = ['id', 'tipo_combustivel', 'identificador_tanque', 'capacidade_maxima', 'quantidade_disponivel']
