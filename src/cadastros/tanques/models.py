c8e3425e140cbd1d10d4fefd0078cbf7464015da
ErickIgles <erick.almeida2125@gmail.com>
2025-08-07 16:39:12 -0300

    feat: tornar campo identificador_tanque opcional com valor padrão 'SEM IDENTIFICADOR'

--- src/cadastros/tanques/models.py
+++ src/cadastros/tanques/models.py
@@ -13,7 +13,7 @@ class Tanque(Base):
     ]
 
     tipo_combustivel = models.CharField(verbose_name='Tipo de Combustivel', max_length=10, choices=TIPO_COMBUSTIVEL_CHOICES)
-    identificador_tanque = models.CharField(verbose_name='Identificador', max_length=7, default='')
+    identificador_tanque = models.CharField(verbose_name='Identificador', max_length=7, blank=True, null=True, default='SEM IDENTIFICADOR')
     capacidade_maxima = models.DecimalField(verbose_name='Capacidade Máxima', max_digits=10, decimal_places=2)
     quantidade_disponivel = models.DecimalField(verbose_name='Quantidade disponivel', max_digits=10, decimal_places=2, default=0.00)
 
