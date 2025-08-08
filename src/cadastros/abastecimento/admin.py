23bbf5bf341a7e23195fb0cee5cfcc5261b3810b
ErickIgles <erick.almeida2125@gmail.com>
2025-08-08 12:54:01 -0300

    feat: registra modelo Combustivel e RegistroAbastecimento com colunas personalizadas no admin

---
+++ src/cadastros/abastecimento/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import Combustivel, RegistroAbastecimento
+
+
+
+@admin.register(Combustivel)
+class CombustivelAdmin(admin.ModelAdmin):    
+    list_display = ['id', 'nome_combustivel', 'valor_base', 'imposto', 'valor_total']
+    list_display_links = ['id', 'nome_combustivel']
+
+
+@admin.register(RegistroAbastecimento)
+class RegistroAbastecimentoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'identificador_tanque', 'tipo_combustivel', 'litros_abastecer', 'valor_total_abastecimento']
+    list_display_links = ['id', 'identificador_tanque']
+
+    def identificador_tanque(self, obj):
+        return obj.tanque.identificador_tanque
+    
