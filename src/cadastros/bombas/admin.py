aa1f12be31e9fbf1d9eb0eac61bea9608de19fcc
ErickIgles <erick.almeida2125@gmail.com>
2025-08-08 09:41:22 -0300

    feat: adiciona metodo para buscar referência de indentificador do tanque

--- src/cadastros/bombas/admin.py
+++ src/cadastros/bombas/admin.py
@@ -4,6 +4,9 @@ from .models import Bomba
 
 @admin.register(Bomba)
 class BombaAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome_bomba', 'tanque']
+    list_display = ['id', 'nome_bomba', 'tanque', 'identificador_tanque']
     list_display_links = ['id', 'nome_bomba', 'tanque']
 
+
+    def identificador_tanque(self, obj):
+        return obj.tanque.identificador_tanque
