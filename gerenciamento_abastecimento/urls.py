91f79c52bb8569beaf421878799bfcf06fc14c63
ErickIgles <erick.almeida2125@gmail.com>
2025-08-08 16:51:43 -0300

    feat: defini base.html como página inicial via TemplateView no app home; atualizada rota raiz

--- gerenciamento_abastecimento/urls.py
+++ gerenciamento_abastecimento/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cadastros.abastecimento.urls')),
+    path('', include('home.urls')),
     path('cadastros/', include('cadastros.urls')),
 ]
 
