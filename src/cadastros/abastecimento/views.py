2139b85661357859759435debb767ff2d730530f
ErickIgles <erick.almeida2125@gmail.com>
2025-08-08 16:38:04 -0300

    refactor: implementa views de cadastro, listagem, atualização e exclusão para RegistroAbastecimento; removidas views antigas comentadas

--- src/cadastros/abastecimento/views.py
+++ src/cadastros/abastecimento/views.py
@@ -1,133 +1,78 @@
-from django.shortcuts import render
-
 from django.core.paginator import Paginator
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DeleteView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
-# from .models import Abastecimento, Tanque, Bomba
-# from .forms import AbastecimentoForm, TanqueForm, BombaForm
-
-
-class IndexTemplateView(TemplateView):
-    template_name = 'base.html'
+from .models import RegistroAbastecimento
+from .forms import AbastecimentoForm
+from cadastros.tanques.models import Tanque
 
 
-# class CriarTanqueView(CreateView):
-#     model = Tanque
-#     form_class = TanqueForm
-#     template_name = 'abastecimento/tanque_form.html'
-#     success_url = reverse_lazy('listagem_tanque')
 
 
-# class ListaTanqueView(ListView):
-#     model = Tanque
-#     context_object_name = 'tanques'
-#     template_name = 'abastecimento/tanque_lista.html'
 
-#     def get_queryset(self):
-#         queryset = super().get_queryset()
-#         q = self.request.GET.get("q")
-#         data_inicio = self.request.GET.get('data_inicio')
-#         data_fim = self.request.GET.get('data_fim')
 
-#         if q:
-#             queryset = queryset.filter(tipo_combustivel__icontains=q)
-#         if data_inicio:
-#             queryset = queryset.filter(criado__gte=data_inicio)
-#         if data_fim:
-#             queryset = queryset.filter(criado__lte=data_fim)    
+
+class RegistroAbastecimentoCadastroView(CreateView):
+    model = RegistroAbastecimento
+    form_class = AbastecimentoForm
+    template_name = 'abastecimento/abastecimento_form.html'
+    success_url = reverse_lazy('cadastros:abastecimento:listar')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tanques'] = Tanque.objects.all()
+        return context
+
+
+class RegitroAbastecimentoListaView(ListView):
+    model = RegistroAbastecimento
+    context_object_name = 'abastecimentos'
+    template_name = 'abastecimento/abastecimento_lista.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        q = self.request.GET.get('q')
+        data_inicio = self.request.GET.get('data_inicio')
+        data_fim = self.request.GET.get('data_fim')
+
+
+        if q:
+            queryset = queryset.filter(bomba__nome_bomba__icontains=q)
+        if data_inicio:
+            queryset = queryset.filter(criado__gte=data_inicio)
+        if data_fim:
+            queryset = queryset.filter(criado__lte=data_fim)
         
-#         return queryset
+        return queryset
     
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         lista_objetos = context.get('object_list')
-#         paginator = Paginator(lista_objetos, 2)
-
-#         page_number = self.request.GET.get('page')
-#         page_obj = paginator.get_page(page_number)
-#         context['page_obj'] = page_obj
-#         return context
-
-
-# class AtualizarTanqueView(UpdateView):
-#     model = Tanque
-#     form_class = TanqueForm
-#     context_object_name = 'tanque'
-#     template_name = 'abastecimento/tanque_form.html'
-#     success_url = reverse_lazy('listagem_tanque')
-
-
-# class DeletarTanqueView(DeleteView):
-#     model = Tanque
-#     context_object_name = 'tanque'
-#     template_name = 'abastecimento/tanque_form_delete.html'
-#     success_url = reverse_lazy('listagem_tanque')
-
-
-
-
-
-# class CriarRegistroAbastecimentoView(CreateView):
-#     model = Abastecimento
-#     form_class = AbastecimentoForm
-#     template_name = 'abastecimento/abastecimento_form.html'
-#     success_url = reverse_lazy('listagem_abastecimento')
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['bombas'] = Bomba.objects.all()
-#         return context
-
-# class ListaRegitroAbastecimentoView(ListView):
-#     model = Abastecimento
-#     context_object_name = 'abastecimentos'
-#     template_name = 'abastecimento/abastecimento_lista.html'
-
-#     def get_queryset(self):
-#         queryset = super().get_queryset()
-#         q = self.request.GET.get('q')
-#         data_inicio = self.request.GET.get('data_inicio')
-#         data_fim = self.request.GET.get('data_fim')
-
-
-#         if q:
-#             queryset = queryset.filter(bomba__nome_bomba__icontains=q)
-#         if data_inicio:
-#             queryset = queryset.filter(criado__gte=data_inicio)
-#         if data_fim:
-#             queryset = queryset.filter(criado__lte=data_fim)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        lista_objetos = context.get('object_list')
         
-#         return queryset
-    
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         lista_objetos = context.get('object_list')
-        
-#         pagination = Paginator(lista_objetos, 1)
+        pagination = Paginator(lista_objetos, 1)
 
-#         page_number = self.request.GET.get('page')
-#         page_obj = pagination.get_page(page_number)
-#         context['page_obj'] = page_obj
-#         return context
+        page_number = self.request.GET.get('page')
+        page_obj = pagination.get_page(page_number)
+        context['page_obj'] = page_obj
+        return context
 
 
+class RegistroAbastecimentoAtualizarView(UpdateView):
+    model = RegistroAbastecimento
+    form_class = AbastecimentoForm
+    context_object_name = 'abastecimento'
+    template_name = 'abastecimento/abastecimento_form_atualizar.html'
+    success_url = reverse_lazy('cadastros:abastecimento:listar')
 
-# class AtualizarRegistroAbastecimentoView(UpdateView):
-#     model = Abastecimento
-#     form_class = AbastecimentoForm
-#     context_object_name = 'abastecimento'
-#     template_name = 'abastecimento/abastecimento_form.html'
-#     success_url = reverse_lazy('listagem_abastecimento')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tanques'] = Tanque.objects.all()
+        return context
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['bombas'] = Bomba.objects.all()
-#         return context
-    
-# class DeletaRegistroAbastecimentoView(DeleteView):
-#     model = Abastecimento
-#     context_object_name = 'abastecimento'
-#     template_name = 'abastecimento/abastecimento_form_delete.html'
-#     success_url = reverse_lazy('listagem_abastecimento')
+
+class RegistroAbastecimentoDeletarView(DeleteView):
+    model = RegistroAbastecimento
+    context_object_name = 'abastecimento'
+    template_name = 'abastecimento/abastecimento_form_delete.html'
+    success_url = reverse_lazy('cadastros:abastecimento:listar')
 
