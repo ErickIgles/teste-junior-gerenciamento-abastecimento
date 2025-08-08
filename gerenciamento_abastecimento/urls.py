2ba24a8b926857163745ad3412e5264d0b507f3b
ErickIgles <erick.almeida2125@gmail.com>
2025-08-08 12:42:58 -0300

    refactor: atualiza o caminho da rota principal

--- gerenciamento_abastecimento/urls.py
+++ gerenciamento_abastecimento/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('abastecimento.urls')),
+    path('', include('cadastros.abastecimento.urls')),
     path('cadastros/', include('cadastros.urls')),
 ]
 
