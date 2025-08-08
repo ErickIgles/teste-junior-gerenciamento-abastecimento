c295e6639a1a1e96f6863f4d732ada296cfbf17a
ErickIgles <erick.almeida2125@gmail.com>
2025-08-08 16:41:47 -0300

    feat: adicionada rota para módulo de abastecimentos no arquivo de URLs de cadastros

--- src/cadastros/urls.py
+++ src/cadastros/urls.py
@@ -5,5 +5,6 @@ app_name='cadastros'
 
 urlpatterns = [
     path('bombas/', include('cadastros.bombas.urls')),
-     path('tanques/', include('cadastros.tanques.urls')),
+    path('tanques/', include('cadastros.tanques.urls')),
+    path('abastecimentos/', include('cadastros.abastecimento.urls')),
 ]
