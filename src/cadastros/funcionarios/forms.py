ec8a8f5e89d2f4c4b77e70d436da607ea192d743
ErickIgles <erick.almeida2125@gmail.com>
2025-08-11 11:48:36 -0300

    feat: adiciona formulário personalizado para atualização de dados de funcionário e tratamento de username e email

--- src/cadastros/funcionarios/forms.py
+++ src/cadastros/funcionarios/forms.py
@@ -32,3 +32,37 @@ class UserForm(UserCreationForm):
             raise forms.ValidationError('E-mail já cadastrado.')
         return email
 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(
+        required=True, 
+        label='E-mail', 
+        widget=forms.EmailInput(
+            attrs={'class': 'form__input', 'placehold':'E-mail'}
+            )
+        )
+    username = forms.CharField(
+        label='Nome de usuário',
+        widget=forms.TextInput(attrs={
+            'class': 'form__input', 'placehold':'Nome de usuário'
+        }))
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        user_id = self.instance.id
+
+        if User.objects.filter(email=email).exclude(pk=user_id).exists():
+            raise forms.ValidationError('Este e-mail já está em uso.')
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        user_id = self.instance.id
+
+        if User.objects.filter(username=username).exclude(pk=user_id).exists():
+            raise forms.ValidationError('Este nome de usuário já está em uso.')
+        return username
