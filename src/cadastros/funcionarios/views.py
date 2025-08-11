a3c1b8d41f406ecdf67f31bf1d5a898240c7eb51
ErickIgles <erick.almeida2125@gmail.com>
2025-08-11 10:22:10 -0300

    feat: adiciona view para cadastro de funcionário

--- src/cadastros/funcionarios/views.py
+++ src/cadastros/funcionarios/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
-# Create your views here.
+from django.urls import reverse_lazy
+from .forms import UserForm
+
+
+
+class FuncionarioCadastrarView(CreateView):
+    template_name = 'funcionarios/form_register.html'
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('home:index')
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
