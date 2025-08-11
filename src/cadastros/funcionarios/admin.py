52c7056b72b4ed173d8107d77b00c6b6bd2266ad
ErickIgles <erick.almeida2125@gmail.com>
2025-08-11 16:38:09 -0300

    feat: adiciona modelos Setor, Cargo e Funcionario ao admin

--- src/cadastros/funcionarios/admin.py
+++ src/cadastros/funcionarios/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Setor, Cargo, Funcionario
+
+
+@admin.register(Setor)
+class SetorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'setor', 'criado', 'modificado', 'ativo']
+    list_display_links = ['id', 'setor']
+    ordering = ['setor', 'criado']
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cargo', 'criado', 'modificado', 'ativo']
+    list_display_links = ['id', 'cargo']
+    ordering = ['cargo', 'criado']
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'cargo', 'setor', 'criado', 'modificado', 'ativo']
+    list_display_links = ['id', 'user']
+    ordering = ['user', 'criado']
+    search_fields = ['user__username', 'cargo__cargo', 'setor__setor']
