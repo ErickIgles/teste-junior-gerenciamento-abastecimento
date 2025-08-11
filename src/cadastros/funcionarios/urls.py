f435a73d5e3e54e644962e2dfdf9e553f10342b2
ErickIgles <erick.almeida2125@gmail.com>
2025-08-11 10:50:13 -0300

    feat: adiciona url para a view de listagem de funcionários

--- src/cadastros/funcionarios/urls.py
+++ src/cadastros/funcionarios/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import FuncionarioCadastrarView
+from .views import (
+    FuncionarioCadastrarView,
+    FuncionarioListarView
+    )
 
 
 app_name = 'funcionarios'
 urlpatterns = [
     path('cadastrar/', FuncionarioCadastrarView.as_view(), name='cadastrar'),
+    path('listar/', FuncionarioListarView.as_view(), name='listar'),
 ]
