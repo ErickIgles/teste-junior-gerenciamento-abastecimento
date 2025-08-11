19db3944b20500623a1867fda24b83294ed728f4
ErickIgles <erick.almeida2125@gmail.com>
2025-08-11 10:22:59 -0300

    feat: adiciona url para a view de cadastro de funcionário

--- src/cadastros/funcionarios/urls.py
+++ src/cadastros/funcionarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .views import FuncionarioCadastrarView
 
+
+app_name = 'funcionarios'
 urlpatterns = [
-
+    path('cadastrar/', FuncionarioCadastrarView.as_view(), name='cadastrar'),
 ]
