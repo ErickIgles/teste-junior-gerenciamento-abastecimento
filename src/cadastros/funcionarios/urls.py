fd3806905971c6a4e9b68892d2c26c8d5eca02a9
ErickIgles <erick.almeida2125@gmail.com>
2025-08-11 11:50:32 -0300

    feat: adiciona url para a view de atualizar dados do funcionário

--- src/cadastros/funcionarios/urls.py
+++ src/cadastros/funcionarios/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from .views import (
     FuncionarioCadastrarView,
-    FuncionarioListarView
+    FuncionarioListarView,
+    FuncionarioAtualizarView,
     )
 
 
@@ -10,4 +11,5 @@ app_name = 'funcionarios'
 urlpatterns = [
     path('cadastrar/', FuncionarioCadastrarView.as_view(), name='cadastrar'),
     path('listar/', FuncionarioListarView.as_view(), name='listar'),
+    path('atualizar/<int:pk>/', FuncionarioAtualizarView.as_view(), name='atualizar'),
 ]
