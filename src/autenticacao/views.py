00cfe342c01511dd5fbc19f6c808e1e300f43ba3
ErickIgles <erick.almeida2125@gmail.com>
2025-08-12 16:43:00 -0300

    feat: adiciona views de login e logout com template e redirecionamento personalizado

--- src/autenticacao/views.py
+++ src/autenticacao/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import UserLoginForm
+
+
+class UsuarioLoginView(LoginView):
+    template_name = 'registration/login.html'
+    redirect_authenticated_user = True
+    form_class = UserLoginForm
+
+    
+class UsuarioLogoutView(LogoutView):
+    next_page = 'home:index'
+
