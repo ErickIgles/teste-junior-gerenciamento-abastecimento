d064379cb8501303c0a751111c557fa32d457260
ErickIgles <erick.almeida2125@gmail.com>
2025-08-12 11:12:56 -0300

    refactor: substitui User por Funcionario nas views de listagem, atualização e exclusão

--- src/cadastros/funcionarios/views.py
+++ src/cadastros/funcionarios/views.py
@@ -5,18 +5,20 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 
 from django.urls import reverse_lazy
-from .forms import UserForm, UserUpdateForm
+
+from .models import Funcionario
+from .forms import FuncionarioForm
 
 
 
 class FuncionarioCadastrarView(CreateView):
     template_name = 'funcionarios/form_register.html'
-    model = User
-    form_class = UserForm
-    success_url = reverse_lazy('cadastros:funcionarios:listar')
+    model = Funcionario
+    form_class = FuncionarioForm
+    success_url = reverse_lazy('home:index')
 
     def form_valid(self, form):
-        messages.success(self.request, f'Funcionário(a) {form.instance.username} cadastrado(a) com sucesso.')
+        messages.success(self.request, f'Funcionário(a) {form.instance.nome_funcionario} cadastrado(a) com sucesso.')
         return super().form_valid(form)
     
     def form_invalid(self, form):
@@ -26,19 +28,18 @@ class FuncionarioCadastrarView(CreateView):
 
 class FuncionarioListarView(ListView):
     template_name = 'funcionarios/lista_funcionario.html'
-    model = User
+    model = Funcionario
     context_object_name = 'funcionarios'
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        q = self.request.GET.get('p')
+        q = self.request.GET.get('q')
         
         data_inicio = self.request.GET.get('data_inicio')
         data_fim = self.request.GET.get('data_fim')
 
         if q:
-            queryset = queryset.filter(username__icontains=q)
-            queryset = queryset.filter(email__icontains=q)
+            queryset = queryset.filter(nome_funcionario__icontains=q)
         if data_inicio:
             queryset = queryset.filter(criado__gte=data_inicio)
         if data_fim:
@@ -56,32 +57,32 @@ class FuncionarioListarView(ListView):
         return context
     
 
-class FuncionarioAtualizarView(UpdateView):
-    template_name = 'funcionarios/form_update.html'
-    model = User
-    context_object_name = 'funcionario'
-    form_class = UserUpdateForm
-    success_url = reverse_lazy('cadastros:funcionarios:listar')
+#class FuncionarioAtualizarView(UpdateView):
+#     template_name = 'funcionarios/form_update.html'
+#     model = Funcionario
+#     context_object_name = 'funcionario'
+#     form_class = FuncionarioForm
+#     success_url = reverse_lazy('cadastros:funcionarios:listar')
 
 
-    def form_valid(self, form):
-        messages.success(self.request, f'Dados do(a) funcionário(a) {form.instance.username} atualizado com sucesso.')
-        return super().form_valid(form)
+#     def form_valid(self, form):
+#         messages.success(self.request, f'Dados do(a) funcionário(a) {form.instance.nome_funcionario} atualizado com sucesso.')
+#         return super().form_valid(form)
     
-    def form_invalid(self, form):
-        messages.error(self.request, f'Erro ao tentar atualizar os dados. Confira as informações.')
-        return super().form_invalid(form)
+#     def form_invalid(self, form):
+#         messages.error(self.request, f'Erro ao tentar atualizar os dados. Confira as informações.')
+#         return super().form_invalid(form)
 
-class FuncionarioDeletarView(DeleteView):
-    template_name = 'funcionarios/form_delete.html'
-    model = User
-    context_object_name = 'funcionario'
-    success_url = reverse_lazy('cadastros:funcionarios:listar')
+# class FuncionarioDeletarView(DeleteView):
+#     template_name = 'funcionarios/form_delete.html'
+#     model = Funcionario
+#     context_object_name = 'funcionario'
+#     success_url = reverse_lazy('cadastros:funcionarios:listar')
 
 
-    def delete(self, request, *args, **kwargs):
-        funcionario = self.get_object()
-        messages.success(self.request, f'Funcionário(a) {funcionario.username} deletado(a) com sucesso')
-        return super().delete(request, *args, **kwargs)
+#     def delete(self, request, *args, **kwargs):
+#         funcionario = self.get_object()
+#         messages.success(self.request, f'Funcionário(a) {funcionario.nome_funcionario} deletado(a) com sucesso')
+#         return super().delete(request, *args, **kwargs)
     
     
