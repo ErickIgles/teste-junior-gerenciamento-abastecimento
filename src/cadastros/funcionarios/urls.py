077cbedc8faecef6e9d6a7f7de8d96a7bbc806b4
ErickIgles <erick.almeida2125@gmail.com>
2025-08-12 13:14:11 -0300

    fix: descomenta rota de atualização de funcionário

--- src/cadastros/funcionarios/urls.py
+++ src/cadastros/funcionarios/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     FuncionarioCadastrarView,
     FuncionarioListarView,
-    # FuncionarioAtualizarView,
+    FuncionarioAtualizarView,
     # FuncionarioDeletarView
     )
 
@@ -12,6 +12,6 @@ app_name = 'funcionarios'
 urlpatterns = [
     path('cadastrar/', FuncionarioCadastrarView.as_view(), name='cadastrar'),
     path('listar/', FuncionarioListarView.as_view(), name='listar'),
-    # path('atualizar/<int:pk>/', FuncionarioAtualizarView.as_view(), name='atualizar'),
-    # # path('deletar/<int:pk>/', FuncionarioDeletarView.as_view(), name='deletar')
+    path('atualizar/<int:pk>/', FuncionarioAtualizarView.as_view(), name='atualizar'),
+    # path('deletar/<int:pk>/', FuncionarioDeletarView.as_view(), name='deletar')
 ]
