ec86469755ee2f46da4b4fa331d7f95d9f4c6ff8
ErickIgles <erick.almeida2125@gmail.com>
2025-08-13 13:30:41 -0300

    feat: cria views para relatórios de abastecimento com listas e detalhes de registros

--- src/relatorios/views.py
+++ src/relatorios/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from django.db.models import Sum
+from cadastros.tanques.models import Tanque
+from cadastros.abastecimento.models import RegistroAbastecimento
+
+
+
+
+class RelatorioAbastecimento(ListView):
+    template_name = 'abastecimentos/relatorio.html'
+    model = RegistroAbastecimento
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tanques = Tanque.objects.all()
+
+        tanques_com_registro = []
+
+        for tanque in tanques:
+
+            if RegistroAbastecimento.objects.filter(tanque=tanque).exists():
+                tanques_com_registro.append(tanque)            
+
+        context['tanques_com_registro'] = tanques_com_registro
+        return context
+
+
+class RelatorioAbastecimentoDetalhe(DetailView):
+    template_name = 'abastecimentos/detalhe.html'
+    model = Tanque
+    context_object_name = 'tanque'
+
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        tanque = self.get_object()
+        registros_tanque = RegistroAbastecimento.objects.filter(tanque=tanque).order_by('criado')
+
+        soma_valores = 0
+
+        for registro in registros_tanque:
+            soma_valores += registro.valor_total_abastecimento
+
+
+        context.update({
+            'registros_tanque': registros_tanque,  # Todos os registros do tanque
+            'soma_valores': soma_valores,  # A soma total dos valores de abastecimento
+        })
+
+        return context
