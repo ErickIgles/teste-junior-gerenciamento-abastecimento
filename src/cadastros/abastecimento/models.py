d5be81483ff0a539819c46864fdd4e2476ae7408
ErickIgles <erick.almeida2125@gmail.com>
2025-08-13 08:44:36 -0300

    feat: adiciona campo 'funcionario', refatora 'litros_abastecer' para 'litros_abastecido' e valida quantidade disponível no tanque

--- src/cadastros/abastecimento/models.py
+++ src/cadastros/abastecimento/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from core.models import Base
 
 from ..tanques.models import Tanque
+from django.contrib.auth.models import User
     
 
 class Combustivel(Base):
@@ -30,10 +31,10 @@ class Combustivel(Base):
 
 
 class RegistroAbastecimento(Base):
-    
+    funcionario = models.ForeignKey(User, verbose_name='Funcionário', on_delete=models.CASCADE, null=False, blank=False)
     tanque = models.ForeignKey(Tanque, verbose_name='Tanque', on_delete=models.CASCADE)
     tipo_combustivel = models.ForeignKey(Combustivel ,verbose_name='Tipo de combustivel', on_delete=models.PROTECT)
-    litros_abastecer = models.DecimalField(verbose_name='Quantidade a abastecer', max_digits=10, decimal_places=2)
+    litros_abastecido = models.DecimalField(verbose_name='Quantidade a abastecida', max_digits=10, decimal_places=2)
     valor_total_abastecimento = models.DecimalField(verbose_name='Valor total', max_digits=10, decimal_places=2, default=0.0)
 
     class Meta:
@@ -41,9 +42,14 @@ class RegistroAbastecimento(Base):
         verbose_name_plural = 'Registros de Combustível'
 
     def save(self, *args, **kwargs):
-        self.valor_total_abastecimento = self.tipo_combustivel.valor_total * self.litros_abastecer
+        self.valor_total_abastecimento = self.tipo_combustivel.valor_total * self.litros_abastecido
+
+        if self.tanque.quantidade_disponivel < self.litros_abastecido:
+            raise ValueError('Quantidade insuficiente no tanque para o abastecimento.')
+        self.tanque.quantidade_disponivel = self.tanque.quantidade_disponivel - self.litros_abastecido
+        self.tanque.save()
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'{self.tipo_combustivel} - {self.litros_abastecer}L - R$ {self.valor_total_abastecimento}'
+        return f'{self.tipo_combustivel} - {self.litros_abastecido}L - R$ {self.valor_total_abastecimento}'
     
