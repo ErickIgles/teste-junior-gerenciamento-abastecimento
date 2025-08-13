a9aa3e2d3d87a0e156b56a8d3ffee5bca3276c40
ErickIgles <erick.almeida2125@gmail.com>
2025-08-13 08:42:42 -0300

    feat: altera campo 'litros_abastecer' para 'litros_abastecido' e adiciona validação no formulário de abastecimento

--- src/cadastros/abastecimento/forms.py
+++ src/cadastros/abastecimento/forms.py
@@ -1,21 +1,38 @@
 from django import forms
 from .models import RegistroAbastecimento
 
+from ..tanques.models import Tanque
 
 class AbastecimentoForm(forms.ModelForm):
 
     class Meta:
         model = RegistroAbastecimento
-        fields = ['tanque', 'tipo_combustivel', 'litros_abastecer']
+        fields = ['tanque', 'tipo_combustivel', 'litros_abastecido']
 
         widgets = {
             'tanque': forms.Select(attrs={'class':'form__select'}),
             'tipo_combustivel': forms.Select(attrs={'class': 'form__select'}),
-            'litros_abastecer': forms.NumberInput(attrs={'class': 'form__input'}),
+            'litros_abastecido': forms.NumberInput(attrs={'class': 'form__input'}),
         }
         labels = {
             'tanque': 'Tanque',
             'tipo_combustivel': 'Tipo de Combustível',
-            'litros_abastecer': 'Litros a Abastecer',
+            'litros_abastecido': 'Litros Abastecido',
         }
 
+
+    def clean_litros_abastecido(self):
+
+        litros_abastecido = self.cleaned_data.get('litros_abastecido')
+        tanque = self.cleaned_data.get('tanque')
+
+
+        if litros_abastecido < 0:
+            raise forms.ValidationError('Por favor, informe um valor positivo.')
+        if litros_abastecido == 0:
+            raise forms.ValidationError('Por favor, informe um valor maior do que 0.')
+        if litros_abastecido > tanque.quantidade_disponivel:
+            raise forms.ValidationError('Valor acima da quantidade disponível no tanque.') 
+        return litros_abastecido
+
+        
