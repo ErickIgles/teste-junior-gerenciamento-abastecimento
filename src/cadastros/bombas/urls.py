e80a3e7c8fbc0c82b9721e46ed77d30fb16ea901
ErickIgles <erick.almeida2125@gmail.com>
2025-08-14 14:09:58 -0300

    fix: corrige nome da URL de listagem para listar nas views de bombas

--- src/cadastros/bombas/urls.py
+++ src/cadastros/bombas/urls.py
@@ -11,7 +11,7 @@ app_name='bombas'
 
 urlpatterns = [
     path('cadastrar/', BombaCadastroView.as_view(), name='cadastrar'),
-    path('listagem/', BombaListarView.as_view(), name='listar'),
+    path('listar/', BombaListarView.as_view(), name='listar'),
     path('atualizar/<int:pk>/', BombaAtualizarView.as_view(), name='atualizar'),
     path('deletar/<int:pk>/', BombaDeletarView.as_view(), name='deletar'),
 ]
