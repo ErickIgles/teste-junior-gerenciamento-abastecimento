753bd5f33c8f9f72e0b337f3b458964e2386c79a
ErickIgles <erick.almeida2125@gmail.com>
2025-08-14 13:49:14 -0300

    feat: adiciona validação personalizada de permissões e grupos nas views de cadastro, listar, atualizar e deletar para abastecimento

--- src/cadastros/abastecimento/views.py
+++ src/cadastros/abastecimento/views.py
@@ -1,13 +1,15 @@
 from django.core.paginator import Paginator
 from django.urls import reverse_lazy
+from django.shortcuts import redirect
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
 
 from .models import RegistroAbastecimento
 from .forms import AbastecimentoForm
 from cadastros.tanques.models import Tanque
-
+from cadastros.funcionarios.models import Funcionario
 
 
 
@@ -21,6 +23,23 @@ class RegistroAbastecimentoCadastroView(LoginRequiredMixin, CreateView):
     template_name = 'abastecimento/abastecimento_form.html'
     success_url = reverse_lazy('cadastros:abastecimento:listar')
 
+
+    def dispatch(self, request, *args, **kwargs):
+        usuario = self.request.user
+
+        if usuario.is_staff:
+            return super().dispatch(request, *args, **kwargs)
+        
+        try:
+            funcionario = Funcionario.objects.get(user=usuario)
+
+            grupos = [grupo.name for grupo in Group.objects.all()]
+            
+            if funcionario.grupo and funcionario.grupo.name in grupos:
+                return super().dispatch(request, *args, **kwargs)
+            return redirect('home:index')
+        except Funcionario.DoesNotExist:
+            return redirect('home:index')
 
     def get_initial(self):
         initial = super().get_initial()
@@ -37,10 +56,27 @@ class RegistroAbastecimentoCadastroView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class RegitroAbastecimentoListaView(ListView):
+class RegitroAbastecimentoListaView(LoginRequiredMixin, ListView):
     model = RegistroAbastecimento
     context_object_name = 'abastecimentos'
     template_name = 'abastecimento/abastecimento_lista.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        usuario = self.request.user
+
+        if usuario.is_staff:
+            return super().dispatch(request, *args, **kwargs)
+        
+        try:
+            funcionario = Funcionario.objects.get(user=usuario)
+
+            grupos = [grupo.name for grupo in Group.objects.all()]
+            
+            if funcionario.grupo and funcionario.grupo.name in grupos:
+                return super().dispatch(request, *args, **kwargs)
+            return redirect('home:index')
+        except Funcionario.DoesNotExist:
+            return redirect('home:index')
 
     def get_queryset(self):
 
@@ -83,6 +119,23 @@ class RegistroAbastecimentoAtualizarView(LoginRequiredMixin, UpdateView):
     template_name = 'abastecimento/abastecimento_form_atualizar.html'
     success_url = reverse_lazy('cadastros:abastecimento:listar')
 
+    def dispatch(self, request, *args, **kwargs):
+        usuario = self.request.user
+
+        if usuario.is_staff:
+            return super().dispatch(request, *args, **kwargs)
+        
+        try:
+            funcionario = Funcionario.objects.get(user=usuario)
+
+            grupos = [grupo.name for grupo in Group.objects.all()]
+            
+            if funcionario.grupo and funcionario.grupo.name in grupos:
+                return super().dispatch(request, *args, **kwargs)
+            return redirect('home:index')
+        except Funcionario.DoesNotExist:
+            return redirect('home:index')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tanques'] = Tanque.objects.all()
@@ -94,4 +147,22 @@ class RegistroAbastecimentoDeletarView(LoginRequiredMixin, DeleteView):
     context_object_name = 'abastecimento'
     template_name = 'abastecimento/abastecimento_form_delete.html'
     success_url = reverse_lazy('cadastros:abastecimento:listar')
+
+
+    def dispatch(self, request, *args, **kwargs):
+        usuario = self.request.user
+
+        if usuario.is_staff:
+            return super().dispatch(request, *args, **kwargs)
+        
+        try:
+            funcionario = Funcionario.objects.get(user=usuario)
+
+            grupos = [grupo.name for grupo in Group.objects.all()]
+            
+            if funcionario.grupo and funcionario.grupo.name in grupos:
+                return super().dispatch(request, *args, **kwargs)
+            return redirect('home:index')
+        except Funcionario.DoesNotExist:
+            return redirect('home:index')
 
