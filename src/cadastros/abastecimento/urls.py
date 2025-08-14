a7bf7c8d590147ec358943ef965c846bc797758c
ErickIgles <erick.almeida2125@gmail.com>
2025-08-14 14:13:09 -0300

    fix: corrige nome da URL de listagem para listar nas views de abastecimento

--- src/cadastros/abastecimento/urls.py
+++ src/cadastros/abastecimento/urls.py
@@ -12,7 +12,7 @@ app_name = 'abastecimento'
 urlpatterns = [
 
     path('cadastrar/', RegistroAbastecimentoCadastroView.as_view(), name='cadastrar'),
-    path('listagem/', RegitroAbastecimentoListaView.as_view(), name='listar'),
+    path('listar/', RegitroAbastecimentoListaView.as_view(), name='listar'),
     path('atualizar/<int:pk>/', RegistroAbastecimentoAtualizarView.as_view(), name='atualizar'),
     path('deletar/<int:pk>/', RegistroAbastecimentoDeletarView.as_view(), name='deletar'),
 ]
