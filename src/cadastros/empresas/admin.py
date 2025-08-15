217b75f05912313cd815f987e76049bda8e23ba2
ErickIgles <erick.almeida2125@gmail.com>
2025-08-15 17:27:32 -0300

    feat: adiciona modelo Empresa ao admin

--- src/cadastros/empresas/admin.py
+++ src/cadastros/empresas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Empresa
+
+
+@admin.register(Empresa)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome_empresa', 'cnpj', 'telefone', 'email', 'usuario_responsavel', 'criado', 'modificado', 'ativo']
+    list_display_links = ['id', 'nome_empresa', 'cnpj']
+    ordering = ('id', )
+    search_fields = ('nome_empresa',)
