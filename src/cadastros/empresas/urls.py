a4d2d3020d1f4057dd9621c8dbb0967f965a8566
ErickIgles <erick.almeida2125@gmail.com>
2025-08-17 20:32:21 -0300

    feat: defini URLs para CBVs de criar, atualizar, visualizar e deletar empresa

--- src/cadastros/empresas/urls.py
+++ src/cadastros/empresas/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
 
+from .views import (
+    EmpresaCriarView,
+    EmpresaAtulizarView,
+    EmpresaPerfilView,
+    EmpresaDeletarView
+)
+
+app_name = 'empresas'
 urlpatterns = [
-    
+    path('cadastrar/', EmpresaCriarView.as_view(), name='cadastrar'),
+    path('atualizar/<int:pk>/', EmpresaAtulizarView.as_view(), name='atualizar'),
+    path('perfil/<int:pk>/', EmpresaPerfilView.as_view(), name='perfil'),
+    path('perfil/deletar/<int:pk>/', EmpresaDeletarView.as_view(), name='deletar'),
 ]
