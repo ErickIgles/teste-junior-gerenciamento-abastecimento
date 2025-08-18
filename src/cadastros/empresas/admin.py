fbb61be84d5c998a2265f68258905775ce21e773
ErickIgles <erick.almeida2125@gmail.com>
2025-08-18 08:45:40 -0300

    feat: adiciona Setor e Cargo ao admin

--- src/cadastros/empresas/admin.py
+++ src/cadastros/empresas/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import Empresa
+from .models import Empresa, Setor, Cargo
 
 
 @admin.register(Empresa)
@@ -10,3 +10,20 @@ class EmpresaAdmin(admin.ModelAdmin):
     list_display_links = ['id', 'nome_empresa', 'cnpj']
     ordering = ('id', )
     search_fields = ('nome_empresa',)
+
+
+@admin.register(Setor)
+class SetorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome_setor', 'empresa']
+    list_display = ['nome_setor']
+    ordering = ('id',)
+    search_fields = ('nome_setor',)
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome_cargo', 'setor']
+    list_display = ['nome_cargo']
+    ordering = ('id',)
+    search_fields = ('nome_cargo',)
+
