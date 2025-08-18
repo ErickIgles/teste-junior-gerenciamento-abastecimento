dd1677426c687c7f4ce26872f842a936e0df7665
ErickIgles <erick.almeida2125@gmail.com>
2025-08-18 09:05:28 -0300

    refactor: remove classes Setor e Cargo e ajusta referência a Cargo

--- src/cadastros/funcionarios/models.py
+++ src/cadastros/funcionarios/models.py
@@ -3,36 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User, Group
 
 from core.models import Base
+from cadastros.empresas.models import Cargo
 
-
-class Setor(Base):
-    setor = models.CharField(max_length=100, verbose_name='Setor')
-
-    class Meta:
-        verbose_name = 'Setor'
-        verbose_name_plural = 'Setores'
-
-    def __str__(self):
-        return f'{self.setor}'
-
-
-class Cargo(Base):
-    cargo = models.CharField(max_length=100, verbose_name='Cargo')
-
-    class Meta:
-        verbose_name = 'Cargo'
-        verbose_name_plural = 'Cargos'
-
-    def __str__(self):
-        return f'{self.cargo}'
 
 
 class Funcionario(Base):
     nome_funcionario = models.CharField(verbose_name='Nome do Funcionário', max_length=100, default='')
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário')
     cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE, verbose_name='Cargo')
-    setor = models.ForeignKey(Setor, on_delete=models.CASCADE, verbose_name='Setor')
-    grupo = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Funcionário'
