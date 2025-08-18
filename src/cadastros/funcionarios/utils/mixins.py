37c024a03a4e9fd98b3032310141b8652024250d
ErickIgles <erick.almeida2125@gmail.com>
2025-08-18 15:32:25 -0300

    feat: cria EmpresaPermissionMixin para restringir acesso aos funcionários baseado no responsável pela empresa

---
+++ src/cadastros/funcionarios/utils/mixins.py
@@ -0,0 +1,22 @@
+from django.contrib import messages
+from django.shortcuts import redirect
+
+
+
+
+
+class EmpresaPermissionMixin:
+    """
+    Restringe acesso aos funcionários da empresa ao usuário responsável pela empresa.
+    """
+    
+    def dispatch(self, request, *args, **kwargs):
+
+        obj = self.get_object()
+        if obj.cargo.setor.empresa.usuario_responsavel != self.request.user:
+            messages.warning(request, 'Você não tem permissão para acessar este registro.')
+            return redirect('home:index')
+
+        return super().dispatch(request, *args, **kwargs)
+    
+    
