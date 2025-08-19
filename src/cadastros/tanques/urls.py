111c69f0cd1d56b57d6d2444290691c049ef75f3
ErickIgles <erick.almeida2125@gmail.com>
2025-08-19 14:40:22 -0300

    feat: adiciona rota de inativação de tanque, desativa rota exclusão. e renome url de listar

--- src/cadastros/tanques/urls.py
+++ src/cadastros/tanques/urls.py
@@ -4,14 +4,16 @@ from .views import (
     TanqueCadastroView,
     TanqueListarView,
     TanqueAtualizarView,
-    TanqueDeletarView
+    # TanqueDeletarView,
+    TanqueInativarView,
 )
 
 app_name = 'tanques'
 
 urlpatterns = [
     path('cadastrar/', TanqueCadastroView.as_view(), name='cadastrar'),
-    path('listagem/', TanqueListarView.as_view(), name='listar'),
+    path('listar/', TanqueListarView.as_view(), name='listar'),
     path('atualizar/<int:pk>/', TanqueAtualizarView.as_view(), name='atualizar'),
-    path('deletar/<int:pk>/', TanqueDeletarView.as_view(), name='deletar'),
+    # path('deletar/<int:pk>/', TanqueDeletarView.as_view(), name='deletar'),
+    path('inativar/<int:pk>/', TanqueInativarView.as_view(), name='inativar'),
 ]
