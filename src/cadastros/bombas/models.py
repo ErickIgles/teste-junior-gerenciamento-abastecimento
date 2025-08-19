0d8aebab7d6b3fc90df86dade537c750d4b28160
ErickIgles <erick.almeida2125@gmail.com>
2025-08-19 16:16:09 -0300

    feat: adiciona relação entre bomba e empresa e tornar tanque opcional

--- src/cadastros/bombas/models.py
+++ src/cadastros/bombas/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 from core.models import Base
 from ..tanques.models import Tanque
+from cadastros.empresas.models import Empresa
 
 
 class Bomba(Base):
-    nome_bomba = models.CharField(verbose_name='Nome', max_length=144)
-    tanque = models.ForeignKey(Tanque, verbose_name='Tanque', on_delete=models.CASCADE)
+    nome_bomba = models.CharField(verbose_name='Nome', max_length=144, unique=True)
+    tanque = models.ForeignKey(Tanque, verbose_name='Tanque', on_delete=models.SET_NULL, null=True, blank=True)
+    empresa = models.ForeignKey(Empresa, verbose_name='Empresa', on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Bomba'
         verbose_name_plural = 'Bombas'
     
     def __str__(self):
-        return f'{self.nome_bomba} - {self.tanque.tipo_combustivel}'
+        return f'{self.nome_bomba} - {self.empresa}'
 
