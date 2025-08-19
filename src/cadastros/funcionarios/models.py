c7456ff98fc0e326695c49e804147ecbef337907
ErickIgles <erick.almeida2125@gmail.com>
2025-08-19 08:59:06 -0300

    style: melhora formatação e organização do modelo Funcionario

--- src/cadastros/funcionarios/models.py
+++ src/cadastros/funcionarios/models.py
@@ -1,16 +1,29 @@
-from django.db import models
 
+from django.db import models
 from django.contrib.auth.models import User
 
 from core.models import Base
-from cadastros.empresas.models import Cargo
-
+from cadastros.empresas.models import (
+    Cargo
+)
 
 
 class Funcionario(Base):
-    nome_funcionario = models.CharField(verbose_name='Nome do Funcionário', max_length=100, default='')
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário')
-    cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE, verbose_name='Cargo')
+    nome_funcionario = models.CharField(
+        verbose_name='Nome do Funcionário', 
+        max_length=100, 
+        default=''
+    )
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        verbose_name='Usuário'
+    )
+    cargo = models.ForeignKey(
+        Cargo, 
+        on_delete=models.CASCADE, 
+        verbose_name='Cargo'
+    )
 
     class Meta:
         verbose_name = 'Funcionário'
