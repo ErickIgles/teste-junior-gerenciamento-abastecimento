c282993a1c2bbb2688d57a5af7fecd77c0451097
ErickIgles <erick.almeida2125@gmail.com>
2025-08-19 16:19:29 -0300

    feat: adiciona campo 'empresa' e mecanismo de busca e ordenação

--- src/cadastros/bombas/admin.py
+++ src/cadastros/bombas/admin.py
@@ -4,8 +4,10 @@ from .models import Bomba
 
 @admin.register(Bomba)
 class BombaAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome_bomba', 'tanque', 'identificador_tanque']
+    list_display = ['id', 'nome_bomba', 'tanque', 'empresa']
     list_display_links = ['id', 'nome_bomba', 'tanque']
+    search_fields = ('nome_bomba',)
+    ordering = ('id',)
 
 
     def identificador_tanque(self, obj):
