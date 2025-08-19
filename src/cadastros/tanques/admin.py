291a2b35c5f33bd50652e13a42366553100a386e
ErickIgles <erick.almeida2125@gmail.com>
2025-08-19 11:22:32 -0300

    feat: adiciona model Combustivel ao admin e adiciona praticas de indentação

--- src/cadastros/tanques/admin.py
+++ src/cadastros/tanques/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
 
-from .models import Tanque
+from .models import Tanque, Combustivel
+
+
+
+@admin.register(Combustivel)
+class CombustivelAdmin(admin.ModelAdmin):    
+    list_display = [
+        'id', 
+        'nome_combustivel', 
+        'valor_base', 
+        'imposto', 
+        'valor_total', 
+        'empresa',
+        'criado',
+        'modificado',
+        'ativo'
+    ]
+    list_display_links = ['id', 'nome_combustivel']
+
 
 
 @admin.register(Tanque)
 class TanqueAdmin(admin.ModelAdmin):
-    list_display = ['id', 'tipo_combustivel', 'identificador_tanque', 'capacidade_maxima', 'quantidade_disponivel']
+    list_display = [
+        'id', 
+        'tipo_combustivel', 
+        'identificador_tanque', 
+        'capacidade_maxima', 
+        'quantidade_disponivel', 
+        'empresa',
+        'criado',
+        'modificado',
+        'ativo'
+    ]
     list_display_links = ['id', 'tipo_combustivel', 'identificador_tanque', 'capacidade_maxima', 'quantidade_disponivel']
