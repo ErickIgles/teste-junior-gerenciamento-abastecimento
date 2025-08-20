efaa0f1fe04671a5383ca1a6cf1536ce065df72d
ErickIgles <erick.almeida2125@gmail.com>
2025-08-20 14:43:25 -0300

    refactor: renomea mixin para EmpresaBombaPermissionMixin e ajustar referência de usuário

--- src/cadastros/bombas/utils/mixins.py
+++ src/cadastros/bombas/utils/mixins.py
@@ -4,13 +4,13 @@ from django.contrib import messages
 
 
 
-class EmpresaTanquePermissionMixin:
+class EmpresaBombaPermissionMixin:
 
     def dispatch(self, request, *args, **kwargs):
 
         obj = self.get_object()
 
-        if obj.empresa.usuario_responsavel != request.user:
+        if obj.empresa.usuario_responsavel != self.request.user:
             messages.error(request, 'Você não possui permissão para acessar este registro.')
             return redirect('home:index')
 
