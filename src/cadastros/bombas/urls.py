3d0760f6a0d99edc52a4b15fd95f502464bebfb7
ErickIgles <erick.almeida2125@gmail.com>
2025-08-20 14:44:53 -0300

    feat: substitui rota deletar por inativar para o recurso bomba

--- src/cadastros/bombas/urls.py
+++ src/cadastros/bombas/urls.py
@@ -4,7 +4,8 @@ from .views import (
     BombaCadastroView,
     BombaListarView,
     BombaAtualizarView,
-    BombaDeletarView
+    # BombaDeletarView,
+    BombaInativarView,
 )
 
 app_name='bombas'
@@ -13,5 +14,7 @@ urlpatterns = [
     path('cadastrar/', BombaCadastroView.as_view(), name='cadastrar'),
     path('listar/', BombaListarView.as_view(), name='listar'),
     path('atualizar/<int:pk>/', BombaAtualizarView.as_view(), name='atualizar'),
-    path('deletar/<int:pk>/', BombaDeletarView.as_view(), name='deletar'),
+    # path('deletar/<int:pk>/', BombaDeletarView.as_view(), name='deletar'),
+    path('inativar/<int:pk>/', BombaInativarView.as_view(), name='inativar'),
+    
 ]
