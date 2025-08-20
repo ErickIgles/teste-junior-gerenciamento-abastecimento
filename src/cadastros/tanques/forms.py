523e9b05e0670b9470370c579af42fd9a5e8f463
ErickIgles <erick.almeida2125@gmail.com>
2025-08-20 16:57:21 -0300

    feat: adicionar tipo_combustivel em TanqueForm e melhorias em validações e filtros por empresa em TanqueUpdateForm

--- src/cadastros/tanques/forms.py
+++ src/cadastros/tanques/forms.py
@@ -1,17 +1,52 @@
 from django import forms
-from .models import Tanque
+from .models import Tanque, Combustivel
+
+
+
+
 
 class TanqueForm(forms.ModelForm):
-    
+    tipo_combustivel = forms.ModelChoiceField(
+        required=False,
+        queryset=Combustivel.objects.none(),
+        widget=forms.Select(
+            attrs={
+                'class': 'form__input',
+            }
+        ),
+        label='Tipo de Combustível'
+    )
+
     class Meta:
         
         model = Tanque
-        fields = ['tipo_combustivel', 'identificador_tanque', 'capacidade_maxima', 'quantidade_disponivel']
+        fields = [
+            'tipo_combustivel', 
+            'identificador_tanque', 
+            'capacidade_maxima', 
+            'quantidade_disponivel'
+            ]
         widgets = {
-            'tipo_combustivel': forms.Select(attrs={'class': 'form__select'}),
-            'identificador_tanque': forms.TextInput(attrs={'class': 'form__input'}),
-            'capacidade_maxima': forms.NumberInput(attrs={'class': 'form__input'}),
-            'quantidade_disponivel': forms.NumberInput(attrs={'class': 'form__input'}),
+            'tipo_combustivel': forms.Select(
+                attrs={
+                    'class': 'form__select'
+                }
+            ),
+            'identificador_tanque': forms.TextInput(
+                attrs={
+                    'class': 'form__input'
+                }
+            ),
+            'capacidade_maxima': forms.NumberInput(
+                attrs={
+                    'class': 'form__input'
+                }
+            ),
+            'quantidade_disponivel': forms.NumberInput(
+                attrs={
+                    'class': 'form__input'
+                }    
+            ),
         }
         labels = {
             'tipo_combustivel': 'Tipo de Combustível',
@@ -20,19 +55,28 @@ class TanqueForm(forms.ModelForm):
             'quantidade_disponivel': 'Quantidade Disponível',
         }
 
+
+    def __init__(self, *args, **kwargs):
+        self.empresa = kwargs.pop('empresa', None)
+        super().__init__(*args, **kwargs)
+
+        if self.empresa:
+            self.fields['tipo_combustivel'].queryset = Combustivel.objects.filter(empresa=self.empresa)
+
     def clean_identificador_tanque(self):
         identificador_tanque = self.cleaned_data.get('identificador_tanque')
 
-        if Tanque.objects.filter(identificador_tanque=identificador_tanque).exists():
+        if Tanque.objects.filter(
+            identificador_tanque=identificador_tanque
+            ).exists():
             raise forms.ValidationError('Identificador já cadastrado.')
         return identificador_tanque
     
 
     def save(self, commit=True):
-        usuario = self.request.user
-
+    
         tanque = self.instance
-        tanque.empresa = usuario
+        tanque.empresa = self.empresa
 
         if commit:
             tanque.save()
@@ -40,19 +84,46 @@ class TanqueForm(forms.ModelForm):
         return tanque
 
 
-
-
 class TanqueUpdateForm(forms.ModelForm):
-    status = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'checkbox'}), label='Status do tanque')
+    status = forms.BooleanField(
+        required=False, 
+        widget=forms.CheckboxInput(
+            attrs={
+                'class': 'checkbox'
+            }
+        ), 
+        label='Status do tanque'
+    )
     class Meta:
         
         model = Tanque
-        fields = ['tipo_combustivel', 'identificador_tanque', 'capacidade_maxima', 'quantidade_disponivel']
+        fields = [
+            'tipo_combustivel', 
+            'identificador_tanque', 
+            'capacidade_maxima', 
+            'quantidade_disponivel'
+        ]
         widgets = {
-            'tipo_combustivel': forms.Select(attrs={'class': 'form__select'}),
-            'identificador_tanque': forms.TextInput(attrs={'class': 'form__input'}),
-            'capacidade_maxima': forms.NumberInput(attrs={'class': 'form__input'}),
-            'quantidade_disponivel': forms.NumberInput(attrs={'class': 'form__input'}),
+            'tipo_combustivel': forms.Select(
+                attrs={
+                    'class': 'form__select'
+                }
+            ),
+            'identificador_tanque': forms.TextInput(
+                attrs={
+                    'class': 'form__input'
+                }
+            ),
+            'capacidade_maxima': forms.NumberInput(
+                attrs={
+                    'class': 'form__input'
+                }
+            ),
+            'quantidade_disponivel': forms.NumberInput(
+                attrs={
+                    'class': 'form__input'
+                }
+            ),
         }
         labels = {
             'tipo_combustivel': 'Tipo de Combustível',
@@ -61,10 +132,25 @@ class TanqueUpdateForm(forms.ModelForm):
             'quantidade_disponivel': 'Quantidade Disponível',
         }
 
+
+        def __init__(self, *args, **kwargs):
+            self.empresa = kwargs.pop('empresa', None)
+            super().__init__(*args, **kwargs)
+
+            if self.empresa:
+                self.fields['tipo_combustivel'].queryset = Combustivel.objects.filter(empresa=self.empresa)
+
+
     def clean_identificador_tanque(self):
         identificador_tanque = self.cleaned_data.get('identificador_tanque')
+        
+        if not self.empresa:
+            raise forms.ValidationError('Empresa não informada para validação')
 
-        if Tanque.objects.filter(identificador_tanque=identificador_tanque).exclude(id=self.instance.pk).exists():
+        if Tanque.objects.filter(
+            identificador_tanque=identificador_tanque, 
+            empresa=self.empresa
+            ).exclude(id=self.instance.pk).exists():
             raise forms.ValidationError('Identificador já cadastrado.')
         return identificador_tanque
     
@@ -74,20 +160,19 @@ class TanqueUpdateForm(forms.ModelForm):
 
 
         if quantidade_disponivel < 0:
-            raise forms.ValidationError('A quantidade não pode ser negativo.')
+            raise forms.ValidationError(
+            'A quantidade não pode ser negativo.'
+                )
         if quantidade_disponivel > capacidade_maxima:
-            raise forms.ValidationError('A quantidade de combustível não pode ser maior que a capacidade máxima do tanque.')
+            raise forms.ValidationError(
+            'A quantidade de combustível não pode ser maior que a capacidade máxima do tanque.'
+            )
         return quantidade_disponivel
 
     def save(self, commit=True):
         
-        usuario = self.instance.empresa
-        tanque = self.instance
-        tanque.empresa = usuario
-        tanque.tipo_combustivel = self.cleaned_data.get('tipo_combustivel')
-        tanque.identificador_tanque = self.cleaned_data.get('identificador_tanque')
-        tanque.capacidade_maxima = self.cleaned_data.get('capacidade_maxima')
-        tanque.quantidade_disponivel = self.cleaned_data.get('quantidade_disponivel')
+        tanque = super().save(commit=False)
+        tanque.empresa = self.empresa
         tanque.ativo = self.cleaned_data.get('status')
 
         if commit:
