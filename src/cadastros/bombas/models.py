3c640d901fc663ebc2f2ff85bada35e2b8050df9
ErickIgles <erick.almeida2125@gmail.com>
2025-08-21 10:27:54 -0300

    style: melhora formatação dos campos do modelo Bomba para maior clareza

--- src/cadastros/bombas/models.py
+++ src/cadastros/bombas/models.py
@@ -6,14 +6,27 @@ from cadastros.empresas.models import Empresa
 
 
 class Bomba(Base):
-    nome_bomba = models.CharField(verbose_name='Nome', max_length=144)
-    tanque = models.ForeignKey(Tanque, verbose_name='Tanque', on_delete=models.SET_NULL, null=True, blank=True)
-    empresa = models.ForeignKey(Empresa, verbose_name='Empresa', on_delete=models.CASCADE, null=True, blank=True)
+    nome_bomba = models.CharField(
+        verbose_name='Nome',
+        max_length=144
+    )
+    tanque = models.ForeignKey(
+        Tanque,
+        verbose_name='Tanque',
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+    empresa = models.ForeignKey(
+        Empresa,
+        verbose_name='Empresa',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
 
     class Meta:
         verbose_name = 'Bomba'
         verbose_name_plural = 'Bombas'
-    
+
     def __str__(self):
         return f'{self.nome_bomba} - {self.empresa}'
-
