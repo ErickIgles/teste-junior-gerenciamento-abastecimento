5445324c4cf70c7960e1ba0c3fc3412c3c4d2095
ErickIgles <erick.almeida2125@gmail.com>
2025-08-21 10:29:54 -0300

    style: reformata urls das views do módulo bombas para maior clareza

--- src/cadastros/bombas/urls.py
+++ src/cadastros/bombas/urls.py
@@ -8,13 +8,28 @@ from .views import (
     BombaInativarView,
 )
 
-app_name='bombas'
+app_name = 'bombas'
 
 urlpatterns = [
-    path('cadastrar/', BombaCadastroView.as_view(), name='cadastrar'),
-    path('listar/', BombaListarView.as_view(), name='listar'),
-    path('atualizar/<int:pk>/', BombaAtualizarView.as_view(), name='atualizar'),
+    path(
+        'cadastrar/',
+        BombaCadastroView.as_view(),
+        name='cadastrar'
+        ),
+    path(
+        'listar/',
+        BombaListarView.as_view(),
+        name='listar'
+        ),
+    path(
+        'atualizar/<int:pk>/',
+        BombaAtualizarView.as_view(),
+        name='atualizar'
+        ),
     # path('deletar/<int:pk>/', BombaDeletarView.as_view(), name='deletar'),
-    path('inativar/<int:pk>/', BombaInativarView.as_view(), name='inativar'),
-    
+    path(
+        'inativar/<int:pk>/',
+        BombaInativarView.as_view(),
+        name='inativar'
+        ),
 ]
