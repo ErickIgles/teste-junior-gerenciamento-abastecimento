3d5d493d85e644712ef919819691cd26d89f7665
ErickIgles <erick.almeida2125@gmail.com>
2025-08-22 13:41:22 -0300

    feat: adiciona campo empresa na model Funcionario com relação opcional

--- src/cadastros/funcionarios/models.py
+++ src/cadastros/funcionarios/models.py
@@ -4,31 +4,38 @@ from django.contrib.auth.models import User
 
 from core.models import Base
 from cadastros.empresas.models import (
-    Cargo
+    Cargo,
+    Empresa
 )
 
 
 class Funcionario(Base):
     nome_funcionario = models.CharField(
-        verbose_name='Nome do Funcionário', 
-        max_length=100, 
+        verbose_name='Nome do Funcionário',
+        max_length=100,
         default=''
     )
     user = models.OneToOneField(
-        User, 
-        on_delete=models.CASCADE, 
+        User,
+        on_delete=models.CASCADE,
         verbose_name='Usuário'
     )
     cargo = models.ForeignKey(
-        Cargo, 
-        on_delete=models.CASCADE, 
+        Cargo,
+        on_delete=models.CASCADE,
         verbose_name='Cargo'
+    )
+    empresa = models.ForeignKey(
+        Empresa,
+        on_delete=models.CASCADE,
+        verbose_name='name',
+        null=True,
+        blank=True
     )
 
     class Meta:
         verbose_name = 'Funcionário'
         verbose_name_plural = 'Funcionários'
-    
+
     def __str__(self):
         return f'{self.nome_funcionario}'
-    
