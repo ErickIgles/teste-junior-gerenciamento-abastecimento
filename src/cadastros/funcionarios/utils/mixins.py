9b3fea52dd4ba4c8c55a43bcaa0665f2a91d855c
ErickIgles <erick.almeida2125@gmail.com>
2025-08-22 17:18:48 -0300

    refactor: ajusta mixin de permissão para usar usuário Funcionario na verificação da empresa responsável

--- src/cadastros/funcionarios/utils/mixins.py
+++ src/cadastros/funcionarios/utils/mixins.py
@@ -1,22 +1,26 @@
 from django.contrib import messages
 from django.shortcuts import redirect
 
-
-
+from ..models import Funcionario
 
 
 class EmpresaPermissionMixin:
     """
-    Restringe acesso aos funcionários da empresa ao usuário responsável pela empresa.
+        Restringe acesso aos funcionários
+        da empresa ao usuário responsável pela empresa.
     """
-    
+
     def dispatch(self, request, *args, **kwargs):
 
-        obj = self.get_object()
-        if obj.cargo.setor.empresa.usuario_responsavel != self.request.user:
-            messages.error(request, 'Você não tem permissão para acessar este registro.')
-            return redirect('home:index')
+        usuario = Funcionario.objects.get(
+            user=request.user
+        )
 
+        obj = self.get_object()
+        if obj.empresa.usuario_responsavel != usuario.empresa.usuario_responsavel:
+            messages.error(
+                request,
+                'Você não tem permissão para acessar este registro.'
+            )
+            return redirect('home:index')
         return super().dispatch(request, *args, **kwargs)
-    
-    
