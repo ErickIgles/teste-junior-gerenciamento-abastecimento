8a4ea5827f612c982943ddf8f88d4b9283e0d75e
ErickIgles <erick.almeida2125@gmail.com>
2025-08-22 17:15:25 -0300

    style: reformatar importações e rotas para maior clareza e consistência

--- src/cadastros/funcionarios/urls.py
+++ src/cadastros/funcionarios/urls.py
@@ -4,16 +4,30 @@ from .views import (
     FuncionarioCadastrarView,
     FuncionarioListarView,
     FuncionarioAtualizarView,
-    # FuncionarioDeletarView,
     FuncionarioInativarView
-    )
+)
 
 
 app_name = 'funcionarios'
 urlpatterns = [
-    path('cadastrar/', FuncionarioCadastrarView.as_view(), name='cadastrar'),
-    path('listar/', FuncionarioListarView.as_view(), name='listar'),
-    path('atualizar/<int:pk>/', FuncionarioAtualizarView.as_view(), name='atualizar'),
-    # path('deletar/<int:pk>/', FuncionarioDeletarView.as_view(), name='deletar')
-    path('inativar/<int:pk>/', FuncionarioInativarView.as_view(), name='inativar'),
+    path(
+        'cadastrar/',
+        FuncionarioCadastrarView.as_view(),
+        name='cadastrar'
+    ),
+    path(
+        'listar/',
+        FuncionarioListarView.as_view(),
+        name='listar'
+    ),
+    path(
+        'atualizar/<int:pk>/',
+        FuncionarioAtualizarView.as_view(),
+        name='atualizar'
+    ),
+    path(
+        'inativar/<int:pk>/',
+        FuncionarioInativarView.as_view(),
+        name='inativar'
+    ),
 ]
