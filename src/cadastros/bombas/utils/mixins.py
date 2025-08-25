d201168b4ef11d34778df79c385a950bfd6d7716
ErickIgles <erick.almeida2125@gmail.com>
2025-08-25 13:43:12 -0300

    feat: adiciona validação de usuário logado e adiciona execução de ações de acordo com a condição de usuário

--- src/cadastros/bombas/utils/mixins.py
+++ src/cadastros/bombas/utils/mixins.py
@@ -1,19 +1,33 @@
 from django.shortcuts import redirect
 from django.contrib import messages
 
-
+from cadastros.funcionarios.models import Funcionario
 
 
 class EmpresaBombaPermissionMixin:
 
     def dispatch(self, request, *args, **kwargs):
 
+        usuario_logado = self.request.user
         obj = self.get_object()
 
-        if obj.empresa.usuario_responsavel != self.request.user:
-            messages.error(request, 'Você não possui permissão para acessar este registro.')
-            return redirect('home:index')
+        if usuario_logado.is_empresa():
+            if obj.empresa.usuario_responsavel != usuario_logado:
+                messages.error(
+                    request,
+                    'Você não tem permissão para acessar este registro.'
+                )
+                return redirect('home:index')
 
+        else:
+            usuario_funcionario = Funcionario.objects.get(
+                user=usuario_logado
+            )
+
+            if obj.empresa.usuario_responsavel != usuario_funcionario.empresa.usuario_responsavel:
+                messages.error(
+                    request,
+                    'Você não tem permissão para acessar este registro.'
+                )
+                return redirect('home:index')
         return super().dispatch(request, *args, **kwargs)
-    
-    
