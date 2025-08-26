798aa291d6b9ff8a93b94c1a4b71769b84be3ebe
ErickIgles <erick.almeida2125@gmail.com>
2025-08-26 12:53:59 -0300

    feat: adiciona mixin para restrinção de acesso apenas à empresa responsável

---
+++ src/cadastros/abastecimento/utils/mixins.py
@@ -0,0 +1,37 @@
+from django.shortcuts import redirect
+from django.contrib import messages
+
+from cadastros.funcionarios.models import Funcionario
+
+
+class EmpresaAbastecimentoPermissionMixin:
+    """
+        Restringe acesso as informações
+        de abastecimento à empresa responsável
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+
+        usuario_logado = self.request.user
+        obj = self.get_object()
+
+        if usuario_logado.is_empresa():
+            if obj.empresa.usuario_responsavel != usuario_logado:
+                messages.error(
+                    request,
+                    'Você não tem permissão para acessar este registro.'
+                )
+                return redirect('home:index')
+
+        else:
+            usuario_funcionario = Funcionario.objects.get(
+                user=usuario_logado
+            )
+
+            if obj.empresa.usuario_responsavel != usuario_funcionario.empresa.usuario_responsavel:
+                messages.error(
+                    request,
+                    'Você não tem permissão para acessar este registro.'
+                )
+                return redirect('home:index')
+        return super().dispatch(request, *args, **kwargs)
