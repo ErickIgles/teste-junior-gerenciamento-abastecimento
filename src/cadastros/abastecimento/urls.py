1f5f275929448b0dfaea23ac13f1a2aa3261f99c
ErickIgles <erick.almeida2125@gmail.com>
2025-08-26 12:48:10 -0300

    style: melhora a formatação das URLs

--- src/cadastros/abastecimento/urls.py
+++ src/cadastros/abastecimento/urls.py
@@ -11,8 +11,24 @@ from .views import (
 app_name = 'abastecimento'
 urlpatterns = [
 
-    path('cadastrar/', RegistroAbastecimentoCadastroView.as_view(), name='cadastrar'),
-    path('listar/', RegitroAbastecimentoListaView.as_view(), name='listar'),
-    path('atualizar/<int:pk>/', RegistroAbastecimentoAtualizarView.as_view(), name='atualizar'),
-    path('deletar/<int:pk>/', RegistroAbastecimentoDeletarView.as_view(), name='deletar'),
+    path(
+        'cadastrar/',
+        RegistroAbastecimentoCadastroView.as_view(),
+        name='cadastrar'
+    ),
+    path(
+        'listar/',
+        RegitroAbastecimentoListaView.as_view(),
+        name='listar'
+    ),
+    path(
+        'atualizar/<int:pk>/',
+        RegistroAbastecimentoAtualizarView.as_view(),
+        name='atualizar'
+    ),
+    path(
+        'deletar/<int:pk>/',
+        RegistroAbastecimentoDeletarView.as_view(),
+        name='deletar'
+    ),
 ]
