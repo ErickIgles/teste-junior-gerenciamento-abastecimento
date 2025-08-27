b514aa3596b0410e6b7b9def1699767c48f4082d
ErickIgles <erick.almeida2125@gmail.com>
2025-08-27 12:59:15 -0300

    feat: adiciona menu hamburguer e refatora lógica de submenus para esperar DOM carregado

--- gerenciamento_abastecimento/static/js/script.js
+++ gerenciamento_abastecimento/static/js/script.js
@@ -1,13 +1,26 @@
-const botoes = document.querySelectorAll('.menu-alternativo');
+document.addEventListener('DOMContentLoaded', () => {
+  const botoes = document.querySelectorAll('.button');
+  
+  for (const botao of botoes) {
+    botao.onclick = () => {
 
-for (const botao of botoes) {
-  botao.onclick = () => {
+      const submenu = botao.nextElementSibling;
+      if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+      } else{
+          submenu.style.display = 'block';
+      }
+    };
+  }
 
-    const submenu = botao.nextElementSibling;
-    if (submenu.style.display === 'block') {
-      submenu.style.display = 'none';
-    } else{
-        submenu.style.display = 'block';
-    }
-  };
-}
+
+  const asidebar = document.querySelector('.asidebar');
+  const hamburguerBtn = document.getElementById('hamburguer-btn');
+  const layout = document.querySelector('.layout');
+
+  hamburguerBtn.addEventListener('click', () => {
+    asidebar.classList.toggle('hidden');
+    layout.classList.toggle('hidden');
+  });
+
+})
