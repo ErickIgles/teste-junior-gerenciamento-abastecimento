b8a98c2a5ecf386850e3fe1082a6f66834dad7d0
ErickIgles <erick.almeida2125@gmail.com>
2025-09-01 14:42:11 -0300

    feat: mantém estado do asidebar usando localStorage

--- gerenciamento_abastecimento/static/gerenciamento_abastecimento/js/script.js
+++ gerenciamento_abastecimento/static/gerenciamento_abastecimento/js/script.js
@@ -18,9 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const hamburguerBtn = document.getElementById('hamburguer-btn');
   const layout = document.querySelector('.layout');
 
+  if (localStorage.getItem('asidebar') === 'aberto'){
+    asidebar.classList.toggle('hidden');
+    layout.classList.toggle('hidden');
+  }
+
   hamburguerBtn.addEventListener('click', () => {
     asidebar.classList.toggle('hidden');
     layout.classList.toggle('hidden');
   });
+
+  if (asidebar.classList.contains('hidden')){
+    localStorage.setItem('asidebar', 'fechado');
+  } else {
+    localStorage.setItem('asidebar', 'aberto')
+  }
 
 })
