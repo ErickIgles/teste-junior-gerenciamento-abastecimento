e145ae18bed22f5d1f1ad285ac5194a5055bb1ce
ErickIgles <erick.almeida2125@gmail.com>
2025-09-02 11:31:00 -0300

    feat: adiciona mixin para autorização de acesso a registro de combustível da empresa referente

--- src/cadastros/tanques/utils/mixins.py
+++ src/cadastros/tanques/utils/mixins.py
@@ -4,9 +4,9 @@ from django.shortcuts import redirect
 from cadastros.funcionarios.models import Funcionario
 
 
-class EmpresaPermissionTanqueMixin:
+class EmpresaTanquePermissionMixin:
     """
-        Restringe a empresa acessas apenas os seus tanques
+        Restringe a empresa acessar apenas os seus tanques
     """
 
     def dispatch(self, request, *args, **kwargs):
@@ -32,4 +32,40 @@ class EmpresaPermissionTanqueMixin:
                 )
                 return redirect('home:index')
 
+        return super().dispatch(request, *args, **kwargs)
+
+
+class EmpresaCombustivelPermissionMixin:
+    """
+    Restringe a empresa acessar apenas os seus combustíveis
+    cadastrados.
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+
+        usuario_logado = self.request.user
+        obj = self.get_object()
+
+        if usuario_logado.is_empresa():
+            if obj.empresa.usuario_responsavel != usuario_logado:
+                messages.error(
+                    request,
+                    """
+                    Você não tem permissão para acessar este registro.
+                    """
+                )
+                return redirect('home:index')
+        else:
+            usuario_funcionario = Funcionario.objects.get(
+                user=usuario_logado
+            )
+
+            if obj.empresa.usuario_responsavel != usuario_funcionario.empresa.usuario_responsavel:
+                messages.error(
+                    request,
+                    """
+                    Você não tem permissão para acessar este registro.
+                    """
+                )
+                return redirect('home:index')
         return super().dispatch(request, *args, **kwargs)
