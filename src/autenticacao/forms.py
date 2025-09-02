3784b5473b2cbb977fc021c95b8c0f148ec0507b
ErickIgles <erick.almeida2125@gmail.com>
2025-09-02 17:02:18 -0300

    feat: adiciona formulário personalizado para login de empresa e muda nome da classe 'UserLoginForm' para 'FuncionarioLoginForm' e adiciona validação de username e password

--- src/autenticacao/forms.py
+++ src/autenticacao/forms.py
@@ -1,16 +1,132 @@
 from django import forms
+from django.contrib.auth import authenticate
 
 from django.contrib.auth.forms import AuthenticationForm
 
+from cadastros.empresas.models import Empresa
+from cadastros.funcionarios.models import Funcionario
 
 
+class FuncionarioLoginForm(AuthenticationForm):
 
-class UserLoginForm(AuthenticationForm):
-    
-    username = forms.CharField(widget=forms.TextInput(attrs={"autofocus": True, 'class': 'form__input'}))
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "autofocus": True,
+                'class': 'form-input'
+            }
+        ),
+        label='Nome de usuário'
+    )
     password = forms.CharField(
         label='Senha',
-        widget=forms.PasswordInput(attrs={"autocomplete": "current-password", 'class': 'form__input'}),
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "current-password",
+                'class': 'form-input'
+            }
+        ),
     )
 
+    def clean(self):
+        cleaned_data = super().clean()
 
+        username = cleaned_data.get(
+            'username'
+        )
+        password = cleaned_data.get(
+            'password'
+        )
+
+        try:
+
+            funcionario = Funcionario.objects.get(
+                nome_funcionario=username
+            )
+
+        except Funcionario.DoesNotExist:
+
+            raise forms.ValidationError(
+                'Funcionário não encontrado.'
+            )
+
+        user_funcionrio = funcionario.user
+
+        if user_funcionrio is None:
+            raise forms.ValidationError(
+                'Nenhum usuário associado a esse funcionário foi encontrado.'
+            )
+
+        user = authenticate(username=user_funcionrio, password=password)
+
+        if user is None:
+            raise forms.ValidationError(
+                'Senha incorreta.'
+            )
+
+        self.user = user
+        self.funcionario = funcionario
+
+        return cleaned_data
+
+
+class EmpresaLoginForm(forms.Form):
+    cnpj = forms.CharField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-input'
+            }
+        ),
+        label='CNPJ'
+    )
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-input'
+            }
+        ),
+        label='Senha'
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        cnpj = cleaned_data.get('cnpj')
+        password1 = cleaned_data.get('password')
+
+        try:
+            empresa = Empresa.objects.get(
+                cnpj=cnpj
+            )
+
+        except Empresa.DoesNotExist:
+
+            raise forms.ValidationError(
+                """
+                Empresa não encontrada.
+                """
+            )
+
+        user = empresa.usuario_responsavel
+
+        if user is None:
+            raise forms.ValidationError(
+                """
+                Nenhum usuário associado a essa empresa foi encontrado.
+                """
+            )
+
+        user = authenticate(username=user.username, password=password1)
+
+        if user is None:
+            raise forms.ValidationError(
+                'Senha incorreta.'
+            )
+
+        self.user = user
+        self.empresa = empresa
+
+        return cleaned_data
