bd4652989c6e55b947a44b5622ca16dcd1f451b7
ErickIgles <erick.almeida2125@gmail.com>
2025-09-02 11:27:26 -0300

    feat: adiciona rotas para views de gerênciamento de combustíveis

--- src/cadastros/tanques/urls.py
+++ src/cadastros/tanques/urls.py
@@ -4,8 +4,11 @@ from .views import (
     TanqueCadastroView,
     TanqueListarView,
     TanqueAtualizarView,
-    # TanqueDeletarView,
-    # TanqueInativarView,
+
+    CombustivelCadastroView,
+    CombustivelListarView,
+    CombustivelAtualizarView,
+    CombustivelDeletarView
 )
 
 app_name = 'tanques'
@@ -25,7 +28,27 @@ urlpatterns = [
         'atualizar/<int:pk>/',
         TanqueAtualizarView.as_view(),
         name='atualizar'
+    ),
+
+    path(
+        'combustiveis/cadastrar/',
+        CombustivelCadastroView.as_view(),
+        name='cadastrar_combustivel',
+    ),
+    path(
+        'combustiveis/listar/',
+        CombustivelListarView.as_view(),
+        name='listar_combustivel'
+    ),
+    path(
+        'combustivel/atualizar/<int:pk>/',
+        CombustivelAtualizarView.as_view(),
+        name='atualizar_combustivel',
+    ),
+    path(
+        'combustivel/deletar/<int:pk>/',
+        CombustivelDeletarView.as_view(),
+        name='deletar_combustivel'
     )
-    # path('deletar/<int:pk>/', TanqueDeletarView.as_view(), name='deletar'),
-    # path('inativar/<int:pk>/', TanqueInativarView.as_view(), name='inativar')
+
 ]
