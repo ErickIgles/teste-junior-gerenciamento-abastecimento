32d2a528b04005298f212f20e9de0e5d585bf5a1
ErickIgles <erick.almeida2125@gmail.com>
2025-09-03 08:20:13 -0300

    refactor: reorganiza código para melhor legibilidade e estrutura

--- src/cadastros/empresas/admin.py
+++ src/cadastros/empresas/admin.py
@@ -6,24 +6,96 @@ from .models import Empresa, Setor, Cargo
 
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome_empresa', 'cnpj', 'telefone', 'email', 'usuario_responsavel', 'criado', 'modificado', 'ativo']
-    list_display_links = ['id', 'nome_empresa', 'cnpj']
-    ordering = ('id', )
+
+    list_display = [
+        'id',
+        'nome_empresa',
+        'cnpj',
+        'telefone',
+        'email',
+        'usuario_responsavel',
+        'criado',
+        'modificado',
+        'ativo'
+    ]
+
+    list_display_links = [
+        'id',
+        'nome_empresa',
+        'cnpj'
+    ]
+
+    ordering = (
+        'id',
+        'criado',
+    )
+
+    list_filter = [
+        'nome_empresa',
+        'cnpj',
+        'email',
+    ]
+
     search_fields = ('nome_empresa',)
 
 
 @admin.register(Setor)
 class SetorAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome_setor', 'empresa']
-    list_display_link = ['id', 'nome_setor']
-    ordering = ('id',)
+
+    list_display = [
+        'id',
+        'nome_setor',
+        'empresa',
+        'criado',
+        'modificado',
+        'ativo'
+    ]
+
+    list_display_link = [
+        'id',
+        'nome_setor'
+    ]
+
+    ordering = (
+        'id',
+        'criado',
+    )
+
+    list_filter = [
+        'nome_setor',
+        'empresa'
+    ]
+
     search_fields = ('nome_setor',)
 
 
 @admin.register(Cargo)
 class CargoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'nome_cargo', 'setor']
-    list_display_link = ['nome_cargo']
-    ordering = ('id',)
-    search_fields = ('nome_cargo',)
 
+    list_display = [
+        'id',
+        'nome_cargo',
+        'setor',
+        'empresa',
+        'criado',
+        'modificado',
+        'ativo'
+    ]
+
+    list_display_link = [
+        'id',
+        'nome_cargo'
+    ]
+
+    ordering = (
+        'id',
+        'criado',
+    )
+
+    list_filter = [
+        'nome_cargo',
+        'setor',
+        'empresa'
+    ]
+
+    search_fields = ('nome_cargo',)
