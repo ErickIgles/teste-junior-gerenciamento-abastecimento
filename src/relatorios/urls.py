1c323cf4343d39da7e788277f16cb349390db9bc
ErickIgles <erick.almeida2125@gmail.com>
2025-09-04 17:44:50 -0300

    refactor: ajusta rotas para relatórios de abastecimento com formatação mais legível

--- src/relatorios/urls.py
+++ src/relatorios/urls.py
@@ -6,6 +6,14 @@ from .views import RelatorioAbastecimento, RelatorioAbastecimentoDetalhe
 
 app_name = 'relatorios'
 urlpatterns = [
-    path('', RelatorioAbastecimento.as_view(), name='listar'),
-    path('detalhes/<int:pk>/', RelatorioAbastecimentoDetalhe.as_view(), name='detalhe'),
+    path(
+        '',
+        RelatorioAbastecimento.as_view(),
+        name='listar'
+    ),
+    path(
+        'detalhes/<int:pk>/',
+        RelatorioAbastecimentoDetalhe.as_view(),
+        name='detalhe'
+    ),
 ]
