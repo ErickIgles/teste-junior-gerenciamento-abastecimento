35a9aa03db22f96cd3563b67b5d0e709321c5840
ErickIgles <erick.almeida2125@gmail.com>
2025-09-05 08:39:50 -0300

    feat: adiciona 'api-auth' para autenticação com Django Rest Framework

--- gerenciamento_abastecimento/urls.py
+++ gerenciamento_abastecimento/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('cadastros/', include('cadastros.urls')),
+
     path('servicos/', include('servicos.urls')),
     path('auth/', include('autenticacao.urls')),
     path('relatorios/', include('relatorios.urls')),
+
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
