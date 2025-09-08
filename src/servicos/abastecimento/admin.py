11e9adbb5f38b1e4f5ef6d0f2054cec29e0ae5f6
ErickIgles <erick.almeida2125@gmail.com>
2025-09-08 16:14:14 -0300

    feat: adiciona modelo 'RegistroReabastecimento' ao admin

--- src/servicos/abastecimento/admin.py
+++ src/servicos/abastecimento/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import RegistroAbastecimento
+from .models import RegistroAbastecimento, RegistroReabastecimento
 
 
 @admin.register(RegistroAbastecimento)
@@ -26,4 +26,28 @@ class RegistroAbastecimentoAdmin(admin.ModelAdmin):
     ordering = (
         'id',
         'criado',
+    )
+
+
+
+@admin.register(RegistroReabastecimento)
+class RegistroReabastecimentoAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'tanque',
+        'quantidade',
+        'funcionario',
+        'empresa',
+        'criado',
+        'modificado',
+        'ativo'
+    ]
+
+    list_display_links = [
+        'id',
+        'tanque',
+    ]
+
+    ordering = (
+        'id',
     )
