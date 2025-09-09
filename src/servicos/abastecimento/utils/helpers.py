fa2aa77b1c02d2cd60b5f230388345bfb36b97eb
ErickIgles <erick.almeida2125@gmail.com>
2025-09-09 18:07:52 -0300

    feat: adiciona função para ajustar página em next_url usando Paginator

--- src/servicos/abastecimento/utils/helpers.py
+++ src/servicos/abastecimento/utils/helpers.py
@@ -1,3 +1,8 @@
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
+
+from django.core.paginator import Paginator
+
+from ..models import RegistroReabastecimento
 
 
 def reverter_reabastecimento(obj):
@@ -14,3 +19,38 @@ def reverter_reabastecimento(obj):
         tanque.quantidade = 0
 
     tanque.save()
+
+
+def ajustar_next_url(next_url):
+
+    registros = RegistroReabastecimento.objects.all().order_by('-criado')
+
+    paginator = Paginator(registros, 6)
+
+    parsed = urlparse(next_url)
+
+    query = parse_qs(parsed.query)
+
+    page = int(query.get('page', ['1'])[0])
+
+    try:
+        page = page
+
+    except (ValueError, TypeError):
+
+        page = 1
+
+    if page >= paginator.num_pages:
+
+        page = paginator.num_pages if paginator.num_pages > 0 else 1
+
+    elif page < 1:
+        page = 1
+
+    query['page'] = [str(page)]
+
+    new_query = urlencode(query, doseq=True)
+
+    new_parsed = parsed._replace(query=new_query)
+
+    return urlunparse(new_parsed)
