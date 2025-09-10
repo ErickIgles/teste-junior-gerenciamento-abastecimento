3f8a487c87c69b0cacd91d6c3364fbb7964cd44f
ErickIgles <erick.almeida2125@gmail.com>
2025-09-10 09:36:44 -0300

    feat: adiciona rotas para autenticação de token; Para geração de QRCode; Validação referente ao reabastecimento; Validação referente ao deletar registro de reabastecimento

--- src/servicos/abastecimento/urls.py
+++ src/servicos/abastecimento/urls.py
@@ -12,6 +12,13 @@ from .views import (
     RegistroReabastecimentoDeletarView,
     )
 
+from .utils.validacao import (
+    gerador_qr,
+    pagina_qr,
+    validacao_token_criar_reabastecimento,
+    validacao_token_deletar_reabastecimento,
+)
+
 
 app_name = 'abastecimento'
 urlpatterns = [
@@ -36,6 +43,7 @@ urlpatterns = [
         RegistroAbastecimentoDeletarView.as_view(),
         name='deletar'
     ),
+
     path(
         'entrada/',
         RegistroReabastecimentoCadastrarView.as_view(),
@@ -50,5 +58,29 @@ urlpatterns = [
         'entrada/deletar/<int:pk>/',
         RegistroReabastecimentoDeletarView.as_view(),
         name='deletar_reabastecimento'
+    ),
+
+    path(
+        'qr/',
+        pagina_qr,
+        name='pagina_qr'
+    ),
+
+    path(
+        'qr-img/',
+        gerador_qr,
+        name='gerador_qr'
+    ),
+    path(
+        'validacao/',
+        validacao_token_criar_reabastecimento,
+        name='validacao_token_cadastrar_reabastecimento'
+    ),
+
+    path(
+        'validar-token/deletar/',
+        validacao_token_deletar_reabastecimento,
+        name='validacao_token_deletar_reabastecimento'
     )
+
 ]
