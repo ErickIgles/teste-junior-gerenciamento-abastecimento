d34962d6a46924a0465e065efa41658191aece73
ErickIgles <erick.almeida2125@gmail.com>
2025-09-11 14:27:29 -0300

    feat: Adiciona o campo 'valor_compra' à listagem do modelo Combustivel no admi; Implementa filtros, campos de busca e ordenação padrão para os modelos Combustivel e Tanque, melhorando a navegabilidade e gerenciamento

--- src/cadastros/tanques/admin.py
+++ src/cadastros/tanques/admin.py
@@ -6,39 +6,71 @@ from .models import Tanque, Combustivel
 
 @admin.register(Combustivel)
 class CombustivelAdmin(admin.ModelAdmin):    
+
     list_display = [
-        'id', 
-        'nome_combustivel', 
-        'valor_base', 
-        'imposto', 
-        'valor_total', 
+        'id',
+        'nome_combustivel',
+        'valor_base',
+        'imposto',
+        'valor_total',
+        'valor_compra',
         'empresa',
         'criado',
         'modificado',
         'ativo'
     ]
+
     list_display_links = [
-        'id', 
-        'nome_combustivel'
+        'id',
+        'nome_combustivel',
     ]
+
+    list_filter = [
+        'nome_combustivel',
+        'empresa'
+    ]
+
+    search_fields = [
+        'nome_combustivel',
+        'empresa',
+    ]
+
+    ordering = (
+        'id',
+    )
 
 
 @admin.register(Tanque)
 class TanqueAdmin(admin.ModelAdmin):
     list_display = [
-        'id', 
-        'tipo_combustivel', 
-        'identificador_tanque', 
-        'capacidade_maxima', 
-        'quantidade_disponivel', 
+        'id',
+        'tipo_combustivel',
+        'identificador_tanque',
+        'capacidade_maxima',
+        'quantidade_disponivel',
         'empresa',
         'criado',
         'modificado',
         'ativo'
     ]
+
+    list_filter = [
+        'tipo_combustivel',
+        'capacidade_maxima',
+        'empresa',
+    ]
+
     list_display_links = [
-        'id', 
-        'tipo_combustivel', 
+        'id',
+        'tipo_combustivel',
         'identificador_tanque',
     ]
 
+    search_fields = [
+        'identificador_tanque',
+        'tipo_combustivel',
+    ]
+
+    ordering = (
+        'id',
+    )
