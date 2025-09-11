7be7ccd46e6e5d136e702075323568a7be8583a6
ErickIgles <erick.almeida2125@gmail.com>
2025-09-11 10:20:13 -0300

    feat: adiciona mixin para validar se a empresa o cargo pertence a empresa que está tentando acessar

--- src/cadastros/empresas/utils/mixins.py
+++ src/cadastros/empresas/utils/mixins.py
@@ -65,3 +65,37 @@ class EmpresaSetorPermissionMixin:
                 )
                 return redirect('home:index')
         return super().dispatch(request, *args, **kwargs)
+
+
+class EmpresaCargoPermissionMixin:
+
+    """
+    Mixin para comparar se o cargo pertence a empresa
+    que está tentando acessar.
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+
+        usuario_logado = self.request.user
+        obj = self.get_object()
+
+        if usuario_logado.is_empresa():
+            if obj.empresa.usuario_responsavel != usuario_logado:
+                messages.error(
+                    request,
+                    'Você não tem permissão para acessar este registro.'
+                )
+                return redirect('home:index')
+
+        else:
+            usuario_funcionario = Funcionario.objects.get(
+                user=usuario_logado
+            )
+
+            if obj.empresa.usuario_responsavel != usuario_funcionario.empresa.usuario_responsavel:
+                messages.error(
+                    request,
+                    'Você não tem permissão para acessar este registro.'
+                )
+                return redirect('home:index')
+        return super().dispatch(request, *args, **kwargs)
