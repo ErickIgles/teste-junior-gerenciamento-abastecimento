78b94886a0f529c878cca71b6bcdaa1f7c982d88
ErickIgles <erick.almeida2125@gmail.com>
2025-09-11 16:53:56 -0300

    feat: implementa o método 'ready' na configuração do app 'Abastecimento' para importar e ativar os signals de gerenciamento de estoque no momento de inicialização do aplicativo

--- src/servicos/abastecimento/apps.py
+++ src/servicos/abastecimento/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AbastecimentoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'servicos.abastecimento'
+
+    def ready(self):
+
+        import servicos.abastecimento.utils.signals
