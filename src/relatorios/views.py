28d1af58ad624cf038ccda45765ec65f4329bdce
ErickIgles <erick.almeida2125@gmail.com>
2025-09-12 15:33:00 -0300

    feat: adiciona views para listagem de abastecimento e reabastecimentos; views para gerar pdf de relatorios de abastecimento e reabastecimentos

--- src/relatorios/views.py
+++ src/relatorios/views.py
@@ -1,3 +1,368 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.template.loader import render_to_string
+
+from django.views.generic import ListView
+from django.db.models import Q, Sum
+
+from servicos.abastecimento.models import (
+    RegistroAbastecimento,
+    RegistroReabastecimento,
+)
+
+from weasyprint import HTML
+
+from cadastros.empresas.models import Empresa
+from cadastros.funcionarios.models import Funcionario
+from cadastros.tanques.models import Combustivel, Tanque
+
+
+class RelatorioAbastecimentos(
+    LoginRequiredMixin,
+
+    ListView
+):
+
+    model = RegistroAbastecimento
+    template_name = 'relatorios/abastecimentos.html'
+
+    def get_user(self):
+
+        usuario_logado = self.request.user
+
+        if usuario_logado.is_empresa():
+
+            empresa = Empresa.objects.select_related(
+                'usuario_responsavel'
+            ).get(
+                usuario_responsavel=usuario_logado
+            )
+
+            return empresa
+
+        funcionario = Funcionario.objects.select_related(
+            'user'
+        ).get(
+            user=usuario_logado
+        )
+
+        empresa = Empresa.objects.select_related(
+            'usuario_responsavel'
+        ).get(
+            usuario_responsavel=funcionario.empresa.usuario_responsavel
+        )
+
+        return empresa
+
+    def apply_filters(self, usuario):
+
+        abastecimentos = RegistroAbastecimento.objects.select_related(
+            'empresa', 'funcionario', 'tipo_combustivel', 'tanque'
+        ).filter(
+            empresa=usuario
+        )
+
+        q = self.request.GET.get('q')
+        data_inicio = self.request.GET.get('data_inicio')
+        data_fim = self.request.GET.get('data_fim')
+        funcionario = self.request.GET.get('funcionario')
+        tipo_combustivel = self.request.GET.get('tipo_combustivel')
+        forma_pagamento = self.request.GET.get('forma_pagamento')
+
+        if q:
+
+            abastecimentos = abastecimentos.filter(
+                Q(funcionario__nome_funcionario__icontains=q) |
+                Q(
+                    tanque__tipo_combustivel__nome_combustivel__icontains=q
+                )
+            )
+
+        if data_inicio and data_fim:
+
+            abastecimentos = abastecimentos.filter(
+                criado__range=[data_inicio, data_fim]
+            )
+
+        if funcionario:
+
+            abastecimentos = abastecimentos.filter(
+                funcionario=funcionario
+            )
+
+        if tipo_combustivel:
+
+            abastecimentos = abastecimentos.filter(
+                tipo_combustivel=tipo_combustivel
+            )
+
+        if forma_pagamento:
+
+            abastecimentos = abastecimentos.filter(
+                forma_pagamento=forma_pagamento
+            )
+
+        abastecimentos = abastecimentos.order_by('-criado')
+
+        return abastecimentos
+
+    def get_queryset(self):
+
+        usuario = self.get_user()
+
+        queryset = self.apply_filters(usuario=usuario)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context['registros'] = self.get_queryset()
+
+        context['tipos_combustivel'] = Combustivel.objects.select_related(
+            'empresa'
+        ).filter(
+            empresa=self.get_user()
+        )
+
+        context['funcionarios'] = Funcionario.objects.select_related(
+            'empresa', 'user'
+        ).filter(
+            empresa=self.get_user()
+        )
+
+        qs = context['registros']
+
+        context['total_litros'] = qs.aggregate(
+            total=Sum('litros_abastecido')
+        )['total'] or 0
+
+        context['total_valor'] = qs.aggregate(
+            total=Sum('valor_total_abastecimento')
+        )['total'] or 0
+
+        return context
+
+
+class RelatorioAbastecimentosPDF(RelatorioAbastecimentos):
+
+    def get(self, request, *args, **kwargs):
+
+        usuario = self.get_user()
+        queryset = self.apply_filters(usuario=usuario)
+
+        totais = queryset.aggregate(
+            total_litros=Sum('litros_abastecido'),
+            total_valor=Sum('valor_total_abastecimento')
+        )
+
+        data_inicio_str = request.GET.get('data_inicio')
+        data_fim_str = request.GET.get('data_fim')
+
+        if data_inicio_str:
+            data_inicio = datetime.strptime(data_inicio_str, '%Y-%m-%d').date()
+        else:
+            data_inicio = None
+
+        if data_fim_str:
+            data_fim = datetime.strptime(data_fim_str, '%Y-%m-%d').date()
+
+        else:
+            data_fim = None
+
+        html_string = render_to_string(
+            'relatorios/abastecimentos_pdf.html',
+            {
+                'abastecimentos': queryset,
+                'total_litros': totais['total_litros'] or 0,
+                'total_valor': totais['total_valor'] or 0,
+                'data_inicio': data_inicio,
+                'data_fim': data_fim
+            }
+        )
+
+        pdf_file = HTML(string=html_string).write_pdf()
+
+        response = HttpResponse(pdf_file, content_type='application/pdf')
+        response['content-Disposition'] = 'attachment; filename="relatorio_abastecimento.pdf"'
+
+        return response
+
+
+class RelatorioReabastecimentos(
+    LoginRequiredMixin,
+
+    ListView
+):
+
+    model = RegistroReabastecimento
+    template_name = 'relatorios/reabastecimentos.html'
+
+    def get_user(self):
+
+        usuario_logado = self.request.user
+
+        if usuario_logado.is_empresa():
+
+            empresa = Empresa.objects.select_related(
+                'usuario_responsavel'
+            ).get(
+                usuario_responsavel=usuario_logado
+            )
+
+            return empresa
+
+        funcionario = Funcionario.objects.select_related(
+            'user'
+        ).get(
+            user=usuario_logado
+        )
+
+        empresa = Empresa.objects.select_related(
+            'usuario_responsavel'
+        ).get(
+            usuario_responsavel=funcionario.empresa.usuario_responsavel
+        )
+
+        return empresa
+
+    def apply_filters(self, usuario):
+
+        reabastecimentos = RegistroReabastecimento.objects.select_related(
+            'empresa', 'funcionario', 'tanque'
+        ).filter(
+            empresa=usuario
+        )
+
+        q = self.request.GET.get('q')
+        data_inicio = self.request.GET.get('data_inicio')
+        data_fim = self.request.GET.get('data_fim')
+        funcionario = self.request.GET.get('funcionario')
+        forma_pagamento = self.request.GET.get('forma_pagamento')
+
+        if q:
+
+            reabastecimentos = reabastecimentos.filter(
+                Q(funcionario__nome_funcionario__icontains=q) |
+                Q(
+                    tanque__tipo_combustivel__nome_combustivel__icontains=q
+                )
+            )
+
+        if data_inicio and data_fim:
+
+            reabastecimentos = reabastecimentos.filter(
+                criado__range=[data_inicio, data_fim]
+            )
+
+        if funcionario:
+
+            reabastecimentos = reabastecimentos.filter(
+                funcionario=funcionario
+            )
+
+        if forma_pagamento:
+
+            reabastecimentos = reabastecimentos.filter(
+                forma_pagamento=forma_pagamento
+            )
+
+            messages.info(
+                self.request,
+                'Não há registros de saída de combustível.'
+            )
+
+        reabastecimentos = reabastecimentos.order_by('-criado')
+
+        return reabastecimentos
+
+    def get_queryset(self):
+
+        usuario = self.get_user()
+
+        queryset = self.apply_filters(usuario=usuario)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context['registros'] = self.get_queryset()
+
+        context['tanques'] = Tanque.objects.select_related(
+            'empresa'
+        ).filter(
+            empresa=self.get_user()
+        )
+
+        context['funcionarios'] = Funcionario.objects.select_related(
+            'empresa', 'user'
+        ).filter(
+            empresa=self.get_user()
+        )
+
+        qs = context['registros']
+
+        context['total_litros'] = qs.aggregate(
+            total=Sum('quantidade')
+        )['total'] or 0
+
+        context['total_valor'] = qs.aggregate(
+            total=Sum('valor_total_reabastecimento')
+        )['total'] or 0
+
+        return context
+
+
+class RelatorioReabastecimentosPDF(
+    RelatorioReabastecimentos
+):
+
+    def get(self, request, *args, **kwargs):
+
+        usuario = self.get_user()
+        queryset = self.apply_filters(usuario=usuario)
+
+        totais = queryset.aggregate(
+            total_litros=Sum('quantidade'),
+            total_valor=Sum('valor_total_reabastecimento')
+        )
+
+        data_inicio_str = request.GET.get('data_inicio')
+        data_fim_str = request.GET.get('data_fim')
+
+        if data_inicio_str:
+            data_inicio = datetime.strptime(data_inicio_str, '%Y-%m-%d').date()
+        else:
+            data_inicio = None
+
+        if data_fim_str:
+            data_fim = datetime.strptime(data_fim_str, '%Y-%m-%d').date()
+
+        else:
+            data_fim = None
+
+        html_string = render_to_string(
+            'relatorios/reabastecimentos_pdf.html',
+            {
+                'reabastecimentos': queryset,
+                'total_litros': totais['total_litros'] or 0,
+                'total_valor': totais['total_valor'] or 0,
+                'data_inicio': data_inicio,
+                'data_fim': data_fim
+            }
+        )
+
+        pdf_file = HTML(string=html_string).write_pdf()
+
+        response = HttpResponse(pdf_file, content_type='application/pdf')
+        response['content-Disposition'] = 'attachment; filename="relatorio_reabastecimento.pdf"'
+
+        return response
