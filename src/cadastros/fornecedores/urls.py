7a6deb2fcfd05b4a630e583860c60923a576f0c1
Erick Almeida <erick.almeida2125@gmail.com>
2025-09-14 22:48:39 -0300

    feat: adiciona URLs para views de listar, cadastrar, editar, deletar do app fornecedores

--- src/cadastros/fornecedores/urls.py
+++ src/cadastros/fornecedores/urls.py
@@ -1,6 +1,32 @@
 from django.urls import path
+from .views import (
+    FornecedorCadastroView,
+    FornecedorListarView,
+    FornecedorAtualizarView,
+    FornecedorDeletarView
+)
 
+app_name = 'fornecedores'
 
 urlpatterns = [
-    
+    path(
+        'listar/',
+        FornecedorListarView.as_view(),
+        name='listar'
+    ),
+    path(
+        'cadastrar/',
+        FornecedorCadastroView.as_view(),
+        name='cadastrar'
+    ),
+    path(
+        'editar/<int:pk>',
+        FornecedorAtualizarView.as_view(),
+        name='editar'
+    ),
+    path(
+        'deletar/<int:pk>/',
+        FornecedorDeletarView.as_view(),
+        name='deletar'
+    )
 ]
