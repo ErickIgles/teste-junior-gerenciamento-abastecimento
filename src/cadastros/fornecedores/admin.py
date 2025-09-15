aa0659c3e2f4d8f9f3ac307f7ba5d1d81475eff2
Erick Almeida <erick.almeida2125@gmail.com>
2025-09-14 22:46:37 -0300

    feat: adiciona modelo Fornecedor ao admin com filtros, ordenação e pesquisa personalizados

--- src/cadastros/fornecedores/admin.py
+++ src/cadastros/fornecedores/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Fornecedor
+
+
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'nome_fantasia',
+        'cnpj',
+        'contato_principal',
+        'telefone',
+        'email',
+        'empresa'
+    ]
+
+    list_display_links = [
+        'nome_fantasia',
+        'cnpj',
+    ]
+
+    list_filter = [
+        'razao_social',
+        'nome_fantasia',
+    ]
+
+    ordering = (
+        'nome_fantasia',
+    )
+
+    search_fields = (
+        'nome_fantasia',
+        'razao_social',
+        'cnpj',
+    )
+
+    list_per_page = 20
