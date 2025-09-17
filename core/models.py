cd1e2b63fd3d5568de7cbcecbe2e532391661c3c
Erick Almeida <erick.almeida2125@gmail.com>
2025-09-17 13:50:50 -0300

    feat: adiciona possibilidade de campos null

--- core/models.py
+++ core/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
+
 class Base(models.Model):
-    criado = models.DateField(verbose_name='Criado', auto_now_add=True)
-    modificado = models.DateTimeField(verbose_name='Atualizado', auto_now=True)
-    ativo = models.BooleanField(verbose_name='Ativo', default=True)
+    criado = models.DateTimeField(
+        verbose_name='Data de Crição',
+        auto_now_add=True,
+        null=True, 
+        blank=True
+    )
+
+    modificado = models.DateTimeField(
+        verbose_name='Data de Modificação',
+        auto_now=True,
+        null=True,
+        blank=True
+    )
+
+    ativo = models.BooleanField(
+        verbose_name='Ativo',
+        default=True
+    )
 
     class Meta:
         abstract = True
-
