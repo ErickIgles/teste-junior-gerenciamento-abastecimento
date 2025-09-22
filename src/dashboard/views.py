4ec8e1c236828b3768dcadc0a247fa5bf952c3fa
Erick Almeida <erick.almeida2125@gmail.com>
2025-09-22 10:49:04 -0300

    fix: corrigi filtragem de dados por empresa

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -5,6 +5,8 @@ from django.db.models import Sum
 from django.utils.timezone import now
 
 from cadastros.tanques.models import Combustivel
+from cadastros.empresas.models import Empresa
+from cadastros.funcionarios.models import Funcionario
 
 from servicos.abastecimento.models import (
     RegistroAbastecimento,
@@ -21,7 +23,7 @@ class DashboardView(
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        filtro = self.request.GET.get("filtro", "mes")  # dia, mes, ano
+        filtro = self.request.GET.get("filtro", "mes")
         combustivel_id = self.request.GET.get("combustivel")
 
         hoje = now().date()
@@ -32,26 +34,39 @@ class DashboardView(
         else:  # mês
             inicio = hoje.replace(day=1)
 
-        # Filtrar abastecimentos e reabastecimentos
-        qs_abastecimentos = RegistroAbastecimento.objects.filter(criado__gte=inicio)
-        qs_reabastecimentos = RegistroReabastecimento.objects.filter(criado__gte=inicio)
+        usuario_logado = self.request.user
+        if usuario_logado.is_empresa():
+            empresa = Empresa.objects.get(usuario_responsavel=usuario_logado)
+        else:
+            usuario_funcionario = Funcionario.objects.get(user=usuario_logado)
+            empresa = usuario_funcionario.empresa
+
+        qs_abastecimentos = RegistroAbastecimento.objects.filter(
+            criado__gte=inicio,
+            bomba__empresa=empresa
+        )
+        qs_reabastecimentos = RegistroReabastecimento.objects.filter(
+            criado__gte=inicio,
+            tanque__empresa=empresa
+        )
 
         if combustivel_id:
-            qs_abastecimentos = qs_abastecimentos.filter(tipo_combustivel_id=combustivel_id)
-            qs_reabastecimentos = qs_reabastecimentos.filter(tanque__tipo_combustivel_id=combustivel_id)
+            qs_abastecimentos = qs_abastecimentos.filter(
+                tipo_combustivel_id=combustivel_id
+            )
+            qs_reabastecimentos = qs_reabastecimentos.filter(
+                tanque__tipo_combustivel_id=combustivel_id
+            )
 
-        # Totais
         abastecimentos_total = qs_abastecimentos.aggregate(Sum("valor_total_abastecimento"))["valor_total_abastecimento__sum"] or 0
         reabastecimentos_total = qs_reabastecimentos.aggregate(Sum("valor_total_reabastecimento"))["valor_total_reabastecimento__sum"] or 0
 
-        # Combustíveis mais vendidos
         combustiveis = (
             qs_abastecimentos.values("tipo_combustivel__nome_combustivel")
             .annotate(total=Sum("litros_abastecido"))
             .order_by("-total")
         )
 
-        # Fornecedores mais pedidos
         fornecedores = (
             qs_reabastecimentos.values("fornecedor__nome_fantasia")
             .annotate(total=Sum("quantidade"))
